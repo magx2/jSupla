674fba04bcffa01eb36f084359bcd47e952445ff
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:46:05 +0200

    TDS_SuplaChannelNewValueResult.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TDS_SuplaChannelNewValueResult.java
@@ -0,0 +1,25 @@
+package pl.grzeslowski.jsupla.proto;
+
+public class TDS_SuplaChannelNewValueResult {
+    /**
+     * unsigned
+     */
+    public final char channelNumber;
+    public final int senderId;
+    public final byte success;
+
+    public TDS_SuplaChannelNewValueResult(char channelNumber, int senderId, byte success) {
+        this.channelNumber = channelNumber;
+        this.senderId = senderId;
+        this.success = success;
+    }
+
+    @Override
+    public String toString() {
+        return "TDS_SuplaChannelNewValueResult{" +
+                "channelNumber=" + channelNumber +
+                ", senderId=" + senderId +
+                ", success=" + success +
+                '}';
+    }
+}
