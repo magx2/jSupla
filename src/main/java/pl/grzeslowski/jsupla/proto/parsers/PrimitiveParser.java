1fdcaf1f4e88f0e8907a96bddcfc0b22fe5522f2
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 15:52:38 +0200

    Adding assert

--- src/main/java/pl/grzeslowski/jsupla/proto/parsers/PrimitiveParser.java
+++ src/main/java/pl/grzeslowski/jsupla/proto/parsers/PrimitiveParser.java
@@ -2,9 +2,12 @@ package pl.grzeslowski.jsupla.proto.parsers;
 
 import static java.lang.Integer.MIN_VALUE;
 import static java.lang.String.format;
+import static pl.grzeslowski.jsupla.consts.JavaConsts.INT_SIZE;
 
 final class PrimitiveParser {
     static int parseInt(byte[] bytes, int offset) {
+        assert bytes.length - offset >= INT_SIZE;
+
         final StringBuilder sb = new StringBuilder();
         for (int i = bytes.length - 1; i >= 0; i--) {
             sb.append(binaryRepresentation(bytes[i]).replace(' ', '0'));
