25f664d2f5e3c8464e1737283398bc028deecfe8
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:35:15 +0200

    TDS_SuplaDeviceChannel_B.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TDS_SuplaDeviceChannel_B.java
@@ -0,0 +1,39 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+/**
+ * @since ver 2
+ */
+public class TDS_SuplaDeviceChannel_B {
+    /**
+     * unsigned
+     */
+    public final byte number;
+    public final int type;
+    public final int funcList;
+    /**
+     * This field name should be default, but this is keyword in Java
+     */
+    public final int default_;
+    public final byte[] value;
+
+    public TDS_SuplaDeviceChannel_B(byte number, int type, int funcList, int default_, byte[] value) {
+        this.number = number;
+        this.type = type;
+        this.funcList = funcList;
+        this.default_ = default_;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "TDS_SuplaDeviceChannel_B{" +
+                "number=" + number +
+                ", type=" + type +
+                ", funcList=" + funcList +
+                ", default_=" + default_ +
+                ", value=" + Arrays.toString(value) +
+                '}';
+    }
+}
