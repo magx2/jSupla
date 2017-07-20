f143f96cb732f4f9aacde0ef7b174d8c8d8b9e62
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:27:41 +0200

    TSC_SuplaLocation.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSC_SuplaLocation.java
@@ -0,0 +1,35 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public final class TSC_SuplaLocation {
+    public final byte eol;
+    public final int id;
+    /**
+     * including the terminating null byte ('\0')
+     *
+     * unsigned
+     */
+    public final int captionSize;
+    /**
+     * Last variable in struct!
+     */
+    public final byte[] caption;
+
+    public TSC_SuplaLocation(byte eol, int id, int captionSize, byte[] caption) {
+        this.eol = eol;
+        this.id = id;
+        this.captionSize = captionSize;
+        this.caption = caption;
+    }
+
+    @Override
+    public String toString() {
+        return "TSC_SuplaLocation{" +
+                "eol=" + eol +
+                ", id=" + id +
+                ", captionSize=" + captionSize +
+                ", caption=" + Arrays.toString(caption) +
+                '}';
+    }
+}
