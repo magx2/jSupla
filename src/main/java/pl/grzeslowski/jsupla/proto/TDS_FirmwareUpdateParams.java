32af1fffb20da1af87e0a953773b671831aeacd5
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 20:02:57 +0200

    TDS_FirmwareUpdateParams.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TDS_FirmwareUpdateParams.java
@@ -0,0 +1,28 @@
+package pl.grzeslowski.jsupla.proto;
+
+public final class TDS_FirmwareUpdateParams {
+    public final byte platform;
+    public final int param1;
+    public final int param2;
+    public final int param3;
+    public final int param4;
+
+    public TDS_FirmwareUpdateParams(byte platform, int param1, int param2, int param3, int param4) {
+        this.platform = platform;
+        this.param1 = param1;
+        this.param2 = param2;
+        this.param3 = param3;
+        this.param4 = param4;
+    }
+
+    @Override
+    public String toString() {
+        return "TDS_FirmwareUpdateParams{" +
+                "platform=" + platform +
+                ", param1=" + param1 +
+                ", param2=" + param2 +
+                ", param3=" + param3 +
+                ", param4=" + param4 +
+                '}';
+    }
+}
