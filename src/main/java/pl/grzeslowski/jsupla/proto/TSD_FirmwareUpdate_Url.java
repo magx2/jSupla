bb3d8670bf933f2ce57f16f08c8e7b74aa3ee88b
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 20:03:57 +0200

    TSD_FirmwareUpdate_Url.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSD_FirmwareUpdate_Url.java
@@ -0,0 +1,27 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public final class TSD_FirmwareUpdate_Url {
+    public final byte avaiableProtocols;
+    public final byte[] host;
+    public final int port;
+    public final byte[] path;
+
+    public TSD_FirmwareUpdate_Url(byte avaiableProtocols, byte[] host, int port, byte[] path) {
+        this.avaiableProtocols = avaiableProtocols;
+        this.host = host;
+        this.port = port;
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "TSD_FirmwareUpdate_Url{" +
+                "avaiableProtocols=" + avaiableProtocols +
+                ", host=" + Arrays.toString(host) +
+                ", port=" + port +
+                ", path=" + Arrays.toString(path) +
+                '}';
+    }
+}
