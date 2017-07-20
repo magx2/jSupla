b5c0c6dd53fdb6da5827e677bd6b75820cfe04c9
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:54:37 +0200

    TCS_SuplaRegisterClient.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TCS_SuplaRegisterClient.java
@@ -0,0 +1,36 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TCS_SuplaRegisterClient {
+    public final int accessId;
+    /**
+     * UTF-8
+     */
+    public final byte[] accessIdPwd;
+    public final byte[] guid;
+    /**
+     * UTF-8
+     */
+    public final byte[] name;
+    public final byte[] softVer;
+
+    public TCS_SuplaRegisterClient(int accessId, byte[] accessIdPwd, byte[] guid, byte[] name, byte[] softVer) {
+        this.accessId = accessId;
+        this.accessIdPwd = accessIdPwd;
+        this.guid = guid;
+        this.name = name;
+        this.softVer = softVer;
+    }
+
+    @Override
+    public String toString() {
+        return "TCS_SuplaRegisterClient{" +
+                "accessId=" + accessId +
+                ", accessIdPwd=" + Arrays.toString(accessIdPwd) +
+                ", guid=" + Arrays.toString(guid) +
+                ", name=" + Arrays.toString(name) +
+                ", softVer=" + Arrays.toString(softVer) +
+                '}';
+    }
+}
