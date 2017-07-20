071903c6b341bea8791372c3c4ac8acb1db2cda4
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:59:34 +0200

    TCS_SuplaChannelNewValue_B.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TCS_SuplaChannelNewValue_B.java
@@ -0,0 +1,21 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TCS_SuplaChannelNewValue_B {
+    public final int channelId;
+    public final byte[] value;
+
+    public TCS_SuplaChannelNewValue_B(int channelId, byte[] value) {
+        this.channelId = channelId;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "TCS_SuplaChannelNewValue_B{" +
+                "channelId=" + channelId +
+                ", value=" + Arrays.toString(value) +
+                '}';
+    }
+}
