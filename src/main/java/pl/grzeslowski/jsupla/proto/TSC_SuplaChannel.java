644e2d50b2c71b84a1812f5b6b252ecd28a831b4
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:51:11 +0200

    TSC_SuplaChannel.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSC_SuplaChannel.java
@@ -0,0 +1,44 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TSC_SuplaChannel {
+    public final byte eol;
+    public final int id;
+    public final int locationId;
+    public final int func;
+    public final byte online;
+    public final TSuplaChannelValue value;
+    /**
+     * Including the terminating null byte ('\0')
+     *
+     * unsigned
+     */
+    public final int captionSize;
+    public final byte[] caption;
+
+    public TSC_SuplaChannel(byte eol, int id, int locationId, int func, byte online, TSuplaChannelValue value, int captionSize, byte[] caption) {
+        this.eol = eol;
+        this.id = id;
+        this.locationId = locationId;
+        this.func = func;
+        this.online = online;
+        this.value = value;
+        this.captionSize = captionSize;
+        this.caption = caption;
+    }
+
+    @Override
+    public String toString() {
+        return "TSC_SuplaChannel{" +
+                "eol=" + eol +
+                ", id=" + id +
+                ", locationId=" + locationId +
+                ", func=" + func +
+                ", online=" + online +
+                ", value=" + value +
+                ", captionSize=" + captionSize +
+                ", caption=" + Arrays.toString(caption) +
+                '}';
+    }
+}
