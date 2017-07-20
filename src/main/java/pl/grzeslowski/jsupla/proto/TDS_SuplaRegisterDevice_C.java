cd0a5f2e5c8b5588d7900ecdc89114d63cea2534
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:41:16 +0200

    TDS_SuplaRegisterDevice_C.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TDS_SuplaRegisterDevice_C.java
@@ -0,0 +1,51 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+/**
+ * @since ver 6
+ */
+public final class TDS_SuplaRegisterDevice_C {
+    public final int locationId;
+    /**
+     * UTF-8
+     */
+    public final byte[] locationPwd;
+    public final byte[] guid;
+    /**
+     * UTF-8
+     */
+    public final byte[] name;
+    public final byte[] softVer;
+    public final byte[] serverName;
+    /**
+     * unsigned
+     */
+    public final byte chennelCount;
+    public final TDS_SuplaDeviceChannel_B[] channels;
+
+    public TDS_SuplaRegisterDevice_C(int locationId, byte[] locationPwd, byte[] guid, byte[] name, byte[] softVer, byte[] serverName, byte chennelCount, TDS_SuplaDeviceChannel_B[] channels) {
+        this.locationId = locationId;
+        this.locationPwd = locationPwd;
+        this.guid = guid;
+        this.name = name;
+        this.softVer = softVer;
+        this.serverName = serverName;
+        this.chennelCount = chennelCount;
+        this.channels = channels;
+    }
+
+    @Override
+    public String toString() {
+        return "TDS_SuplaRegisterDevice_C{" +
+                "locationId=" + locationId +
+                ", locationPwd=" + Arrays.toString(locationPwd) +
+                ", guid=" + Arrays.toString(guid) +
+                ", name=" + Arrays.toString(name) +
+                ", softVer=" + Arrays.toString(softVer) +
+                ", serverName=" + Arrays.toString(serverName) +
+                ", chennelCount=" + chennelCount +
+                ", channels=" + Arrays.toString(channels) +
+                '}';
+    }
+}
