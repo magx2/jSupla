235d0e5b54e145a631922b40e13977bfe9d7bb5f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 13:51:37 +0200

    Starting HTTPs server

--- src/main/java/pl/grzeslowski/jsupla/server/Server.java
+++ src/main/java/pl/grzeslowski/jsupla/server/Server.java
@@ -1,29 +1,62 @@
 package pl.grzeslowski.jsupla.server;
 
-import pl.grzeslowski.jsupla.consts.ProtoConsts;
 import pl.grzeslowski.jsupla.proto.TSuplaDataPacket;
 
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Arrays;
 
+import static java.lang.String.format;
 import static pl.grzeslowski.jsupla.consts.ProtoConsts.SUPLA_MAX_DATA_SIZE;
 
 public class Server implements AutoCloseable {
     private final int port;
     private ServerSocketChannel serverSocketChannel;
 
+    private Server(int port) {
+        this.port = port;
+    }
+
     public static void main(String[] args) throws Exception {
         try (Server server = new Server(2016)) {
-            server.start();
+            server.startHttpsServer();
         }
     }
 
-    private Server(int port) {
-        this.port = port;
+    private void startHttpsServer() throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", Server.class.getClassLoader().getResource("jsupla-keystore").getFile());
+        System.setProperty("javax.net.ssl.keyStorePassword","jSuplaPwd");
+
+        System.out.println("Starting server on port " + port);
+        SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+        ServerSocket ss = ssf.createServerSocket(port);
+
+        while (true) {
+            Socket s = ss.accept();
+            OutputStream out = s.getOutputStream();
+            InputStream in = s.getInputStream();
+
+            byte[] bytes = new byte[16 * 1024];
+
+            int count;
+            while ((count = in.read(bytes)) > 0) {
+                out.write(bytes, 0, count);
+                final byte[] array = Arrays.copyOf(bytes, count);
+                for (int i = 0; i < array.length; i++) {
+                    System.out.print(format("%8s", Integer.toBinaryString(array[i] & 0xFF)).replace(' ', '0')); // 00000010
+//                    System.out.printf("%4d", array[i]);
+                    System.out.print(" ");
+                }
+            }
+        }
     }
 
     private void start() throws IOException {
@@ -45,7 +78,7 @@ public class Server implements AutoCloseable {
 //                System.out.println("Bytes read: " + bytesRead);
                 final byte[] array = buf.array();
                 for (int i = 0; i < array.length && i < bytesRead; i++) {
-                    String s2 = String.format("%8s", Integer.toBinaryString(array[i] & 0xFF)).replace(' ', '0');
+                    String s2 = format("%8s", Integer.toBinaryString(array[i] & 0xFF)).replace(' ', '0');
                     System.out.print(s2); // 00000010
 //                    System.out.printf("%4d", array[i]);
                     System.out.print(" ");
