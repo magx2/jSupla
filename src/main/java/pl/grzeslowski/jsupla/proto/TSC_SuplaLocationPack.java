84bdb2f12368fb2a0c224eef06f03702f2771ae0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:28:56 +0200

    TSC_SuplaLocationPack.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSC_SuplaLocationPack.java
@@ -0,0 +1,24 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TSC_SuplaLocationPack {
+    public final int count;
+    public final int totalLeft;
+    public final TSC_SuplaLocation[] locations;
+
+    public TSC_SuplaLocationPack(int count, int totalLeft, TSC_SuplaLocation[] locations) {
+        this.count = count;
+        this.totalLeft = totalLeft;
+        this.locations = locations;
+    }
+
+    @Override
+    public String toString() {
+        return "TSC_SuplaLocationPack{" +
+                "count=" + count +
+                ", totalLeft=" + totalLeft +
+                ", locations=" + Arrays.toString(locations) +
+                '}';
+    }
+}
