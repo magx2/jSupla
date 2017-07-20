b5394d839983761770cdfc315262b7220a2ed3c8
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:23:27 +0200

    TSDC_SuplaSetActivityTimeoutResult.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSDC_SuplaSetActivityTimeoutResult.java
@@ -0,0 +1,31 @@
+package pl.grzeslowski.jsupla.proto;
+
+public class TSDC_SuplaSetActivityTimeoutResult {
+    /**
+     * unsigned
+     */
+    public final byte activityTimeout;
+    /**
+     * unsigned
+     */
+    public final byte min;
+    /**
+     * unsigned
+     */
+    public final byte max;
+
+    public TSDC_SuplaSetActivityTimeoutResult(byte activityTimeout, byte min, byte max) {
+        this.activityTimeout = activityTimeout;
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public String toString() {
+        return "TSDC_SuplaSetActivityTimeoutResult{" +
+                "activityTimeout=" + activityTimeout +
+                ", min=" + min +
+                ", max=" + max +
+                '}';
+    }
+}
