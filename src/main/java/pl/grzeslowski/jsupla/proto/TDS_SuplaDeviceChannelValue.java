b6d8f3873efd3a60a96a73d9a9dd09d643bc6f95
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:43:23 +0200

    TDS_SuplaDeviceChannelValue.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TDS_SuplaDeviceChannelValue.java
@@ -0,0 +1,24 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TDS_SuplaDeviceChannelValue {
+    /**
+     * unsigned
+     */
+    public final byte channelNumber;
+    public final byte[] value;
+
+    public TDS_SuplaDeviceChannelValue(byte channelNumber, byte[] value) {
+        this.channelNumber = channelNumber;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "TDS_SuplaDeviceChannelValue{" +
+                "channelNumber=" + channelNumber +
+                ", value=" + Arrays.toString(value) +
+                '}';
+    }
+}
