eb9600b3b7c1bc8e3d8ddac7320c1dbaf3932323
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:49:14 +0200

    TSC_SuplaChannelValue.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSC_SuplaChannelValue.java
@@ -0,0 +1,25 @@
+package pl.grzeslowski.jsupla.proto;
+
+public class TSC_SuplaChannelValue {
+    public final byte eol;
+    public final int id;
+    public final byte online;
+    public final TSuplaChannelValue value;
+
+    public TSC_SuplaChannelValue(byte eol, int id, byte online, TSuplaChannelValue value) {
+        this.eol = eol;
+        this.id = id;
+        this.online = online;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "TSC_SuplaChannelValue{" +
+                "eol=" + eol +
+                ", id=" + id +
+                ", online=" + online +
+                ", value=" + value +
+                '}';
+    }
+}
