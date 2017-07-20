98de16c6c97eec058bdabdf768ed53a24b75ecfa
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-20 19:52:13 +0200

    TSC_SuplaChannelPack.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/TSC_SuplaChannelPack.java
@@ -0,0 +1,24 @@
+package pl.grzeslowski.jsupla.proto;
+
+import java.util.Arrays;
+
+public class TSC_SuplaChannelPack {
+    public final int count;
+    public final int totalLeft;
+    public final TSC_SuplaChannel[] channels;
+
+    public TSC_SuplaChannelPack(int count, int totalLeft, TSC_SuplaChannel[] channels) {
+        this.count = count;
+        this.totalLeft = totalLeft;
+        this.channels = channels;
+    }
+
+    @Override
+    public String toString() {
+        return "TSC_SuplaChannelPack{" +
+                "count=" + count +
+                ", totalLeft=" + totalLeft +
+                ", channels=" + Arrays.toString(channels) +
+                '}';
+    }
+}
