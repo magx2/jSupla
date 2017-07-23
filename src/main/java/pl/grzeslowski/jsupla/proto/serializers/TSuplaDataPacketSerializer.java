0e04addcb05649ba24d4ea3b0da71a2eb7cfb73e
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-23 22:03:11 +0200

    Using put byte

--- src/main/java/pl/grzeslowski/jsupla/proto/serializers/TSuplaDataPacketSerializer.java
+++ src/main/java/pl/grzeslowski/jsupla/proto/serializers/TSuplaDataPacketSerializer.java
@@ -4,6 +4,7 @@ import pl.grzeslowski.jsupla.proto.structs.TSuplaDataPacket;
 
 import static java.lang.System.arraycopy;
 import static pl.grzeslowski.jsupla.consts.JavaConsts.INT_SIZE;
+import static pl.grzeslowski.jsupla.proto.serializers.PrimitiveSerizaliser.putUnsignedByte;
 import static pl.grzeslowski.jsupla.proto.serializers.PrimitiveSerizaliser.putUnsignedInt;
 
 public final class TSuplaDataPacketSerializer implements Serializer<TSuplaDataPacket> {
@@ -11,7 +12,7 @@ public final class TSuplaDataPacketSerializer implements Serializer<TSuplaDataPa
     public byte[] serialise(TSuplaDataPacket proto) {
         final byte[] bytes = new byte[proto.size()];
 
-        putUnsignedInt(proto.version, bytes, 0);
+        putUnsignedByte(proto.version, bytes, 0);
         putUnsignedInt(proto.rrId, bytes, INT_SIZE);
         putUnsignedInt(proto.callType, bytes, INT_SIZE * 2);
         arraycopy(proto.data, 0, bytes, INT_SIZE * 3, proto.data.length);
