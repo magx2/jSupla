0f8980d102a670a7a7b292666dc69db48fbadd19
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-23 23:19:47 +0200

    Serializer and parser

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/parsers/TDS_SuplaRegisterDevice_BParser.java
@@ -0,0 +1,12 @@
+package pl.grzeslowski.jsupla.proto.parsers;
+
+import pl.grzeslowski.jsupla.proto.structs.TDS_SuplaRegisterDevice_B;
+
+@Deprecated
+@SuppressWarnings("DeprecatedIsStillUsed")
+public class TDS_SuplaRegisterDevice_BParser implements Parser<TDS_SuplaRegisterDevice_B> {
+    @Override
+    public TDS_SuplaRegisterDevice_B parse(byte[] bytes) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/pl/grzeslowski/jsupla/proto/serializers/TDS_SuplaRegisterDevice_BSerializer.java
@@ -0,0 +1,12 @@
+package pl.grzeslowski.jsupla.proto.serializers;
+
+import pl.grzeslowski.jsupla.proto.structs.TDS_SuplaRegisterDevice_B;
+
+@Deprecated
+@SuppressWarnings("DeprecatedIsStillUsed")
+public class TDS_SuplaRegisterDevice_BSerializer implements Serializer<TDS_SuplaRegisterDevice_B> {
+    @Override
+    public byte[] serialise(TDS_SuplaRegisterDevice_B proto) {
+        throw new UnsupportedOperationException();
+    }
+}
