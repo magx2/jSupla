58d4afcdc466e73b8a05e452f717f9be5257b1d3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-23 21:42:55 +0200

    TSuplaDataPacketSerializer.java

---
+++ src/main/java/pl/grzeslowski/jsupla/proto/serializers/TSuplaDataPacketSerializer.java
@@ -0,0 +1,21 @@
+package pl.grzeslowski.jsupla.proto.serializers;
+
+import pl.grzeslowski.jsupla.proto.structs.TSuplaDataPacket;
+
+import static java.lang.System.arraycopy;
+import static pl.grzeslowski.jsupla.consts.JavaConsts.INT_SIZE;
+import static pl.grzeslowski.jsupla.proto.serializers.PrimitiveSerizaliser.putUnsignedInt;
+
+public class TSuplaDataPacketSerializer implements Serializer<TSuplaDataPacket> {
+    @Override
+    public byte[] serialise(TSuplaDataPacket proto) {
+        final byte[] bytes = new byte[proto.size()];
+
+        putUnsignedInt(proto.version, bytes, 0);
+        putUnsignedInt(proto.rrId, bytes, INT_SIZE);
+        putUnsignedInt(proto.callType, bytes, INT_SIZE * 2);
+        arraycopy(proto.data, 0, bytes, INT_SIZE * 3, proto.data.length);
+
+        return bytes;
+    }
+}
