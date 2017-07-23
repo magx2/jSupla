2e0d8e53707d49b66b9423e91a6a472e7790a22d
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-23 22:02:21 +0200

    Removing old code

--- src/main/java/pl/grzeslowski/jsupla/proto/serializers/PrimitiveSerizaliser.java
+++ src/main/java/pl/grzeslowski/jsupla/proto/serializers/PrimitiveSerizaliser.java
@@ -6,7 +6,6 @@ import static pl.grzeslowski.jsupla.consts.JavaConsts.INT_SIZE;
 
 final class PrimitiveSerizaliser {
 
-    @SuppressWarnings("PointlessArithmeticExpression")
     public static void putUnsignedInt(int value, byte[] bytes, int offset) {
         if (bytes.length - offset < INT_SIZE) {
             throw new IllegalArgumentException(format("Bytes array has length %s and is not sufficient to put here int (%s) with offset %s",
