0cb19a62e866f0c916022ecbb7cc3efc8dc22412
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-25 00:42:33 +0200

    Adding constrains on byte fields

--- src/main/java/pl/grzeslowski/jsupla/server/entities/DeviceChannel.java
+++ src/main/java/pl/grzeslowski/jsupla/server/entities/DeviceChannel.java
@@ -1,16 +1,17 @@
 package pl.grzeslowski.jsupla.server.entities;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Arrays;
 
-import static pl.grzeslowski.jsupla.Preconditions.min;
-import static pl.grzeslowski.jsupla.Preconditions.sizeMax;
+import static pl.grzeslowski.jsupla.Preconditions.*;
 import static pl.grzeslowski.jsupla.consts.ProtoConsts.SUPLA_CHANNELVALUE_SIZE;
 
 public class DeviceChannel implements Entity {
     @Min(0)
+    @Max(255)
     private final int number;
     @Min(0)
     private final int type;
@@ -19,7 +20,7 @@ public class DeviceChannel implements Entity {
     private final byte[] value; // TODO should be some object that can be mapped from `type`
 
     public DeviceChannel(int number, int type, byte[] value) {
-        this.number = min(number, 0);
+        this.number = size(number, 0, 255);
         this.type = min(type, 0);
         this.value = sizeMax(value, SUPLA_CHANNELVALUE_SIZE);
     }
--- src/main/java/pl/grzeslowski/jsupla/server/entities/RegisterDeviceResult.java
+++ src/main/java/pl/grzeslowski/jsupla/server/entities/RegisterDeviceResult.java
@@ -1,25 +1,30 @@
 package pl.grzeslowski.jsupla.server.entities;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 import static java.lang.String.format;
 import static pl.grzeslowski.jsupla.Preconditions.min;
+import static pl.grzeslowski.jsupla.Preconditions.size;
 
 public class RegisterDeviceResult implements Entity {
     @Min(0)
     private final int resultCode;
     @Min(0)
+    @Max(255)
     private final int activityTimeout;
     @Min(0)
+    @Max(255)
     private final int version;
     @Min(0)
+    @Max(255)
     private final int versionMin;
 
     public RegisterDeviceResult(int resultCode, int activityTimeout, int version, int versionMin) {
         this.resultCode = min(resultCode, 0);
-        this.activityTimeout = min(activityTimeout, 0);
-        this.version = min(version, 0);
-        this.versionMin = min(versionMin, 0);
+        this.activityTimeout = size(activityTimeout, 0, 255);
+        this.version = size(version, 0, 255);
+        this.versionMin = size(versionMin, 0, 255);
         if (versionMin > version) {
             throw new IllegalArgumentException(format("Min version %s can not be larger that version %s!", versionMin, version));
         }
