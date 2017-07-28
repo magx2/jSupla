0268b14cdc8f6a0b4c653e0c2f7c1e4f89e72939
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-28 09:20:12 +0200

    DeviceRegisterRequestBParser.java

---
+++ src/main/java/pl/grzeslowski/jsupla/server/parsers/DeviceRegisterRequestBParser.java
@@ -0,0 +1,30 @@
+package pl.grzeslowski.jsupla.server.parsers;
+
+import pl.grzeslowski.jsupla.proto.structs.ds.TDS_SuplaRegisterDevice_B;
+import pl.grzeslowski.jsupla.server.entities.DeviceChannelB;
+import pl.grzeslowski.jsupla.server.entities.requests.DeviceRegisterRequestB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static pl.grzeslowski.jsupla.proto.decoders.PrimitiveParser.parseHexString;
+import static pl.grzeslowski.jsupla.proto.decoders.PrimitiveParser.parseUtf8String;
+
+public class DeviceRegisterRequestBParser implements Parser<DeviceRegisterRequestB, TDS_SuplaRegisterDevice_B> {
+    @Override
+    public Class<TDS_SuplaRegisterDevice_B> getProtoClass() {
+        return TDS_SuplaRegisterDevice_B.class;
+    }
+
+    @Override
+    public DeviceRegisterRequestB parse(TDS_SuplaRegisterDevice_B proto) {
+        int locationId = proto.locationId;
+        String locationPassword = parseUtf8String(proto.locationPwd);
+        String guid = parseHexString(proto.guid);
+        String name = parseUtf8String(proto.name);
+        String softVersion = parseUtf8String(proto.softVer);
+        List<? extends DeviceChannelB> channels = new ArrayList<>();// TODO
+
+        return new DeviceRegisterRequestB(locationId, locationPassword, guid, name, softVersion, channels);
+    }
+}
--- src/main/java/pl/grzeslowski/jsupla/server/parsers/Parser.java
+++ src/main/java/pl/grzeslowski/jsupla/server/parsers/Parser.java
@@ -4,7 +4,7 @@ import pl.grzeslowski.jsupla.proto.structs.ds.DeviceServer;
 import pl.grzeslowski.jsupla.server.entities.requests.Request;
 
 public interface Parser<Rq extends Request, DS extends DeviceServer> {
-    DS getProtoClass();
+    Class<DS> getProtoClass();
 
     Rq parse(DS proto);
 }
