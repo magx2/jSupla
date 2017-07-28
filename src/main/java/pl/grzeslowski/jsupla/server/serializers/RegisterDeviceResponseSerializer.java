e303fc97cc0b21c1a85e0569e27469e57db3f206
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-07-28 10:43:55 +0200

    RegisterDeviceResponseSerializer

---
+++ src/main/java/pl/grzeslowski/jsupla/server/serializers/RegisterDeviceResponseSerializer.java
@@ -0,0 +1,16 @@
+package pl.grzeslowski.jsupla.server.serializers;
+
+import pl.grzeslowski.jsupla.proto.structs.sd.TSD_SuplaRegisterDeviceResult;
+import pl.grzeslowski.jsupla.server.entities.responses.RegisterDeviceResponse;
+
+class RegisterDeviceResponseSerializer implements Serializer<RegisterDeviceResponse, TSD_SuplaRegisterDeviceResult> {
+    @Override
+    public TSD_SuplaRegisterDeviceResult serialize(RegisterDeviceResponse entity) {
+        int resultCode = entity.getResultCode();
+        byte activityTimeout = (byte) entity.getActivityTimeout();
+        byte version = (byte) entity.getVersion();
+        byte versionMin = (byte) entity.getVersionMin();
+
+        return new TSD_SuplaRegisterDeviceResult(resultCode, activityTimeout, version, versionMin);
+    }
+}
