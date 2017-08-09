1d3294ed18065c75544959eb37914f34bed3291d
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:42:55 +0200

    `Too long line`

--- server/src/main/java/pl/grzeslowski/jsupla/server/listeners/ListenersFactory.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/listeners/ListenersFactory.java
@@ -4,5 +4,5 @@ import pl.grzeslowski.jsupla.server.entities.requests.Request;
 import pl.grzeslowski.jsupla.server.entities.responses.Response;
 
 public interface ListenersFactory {
-    <RequestT extends Request, ResponseT extends Response> RequestListener<RequestT, ResponseT> getRequestListener(RequestT request);
+    <RqT extends Request, RspT extends Response> RequestListener<RqT, RspT> getRequestListener(RqT request);
 }
