0f9785e637f3024842ed0f5b4500861af22242ed
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:44:04 +0200

    Long line

--- server/src/main/java/pl/grzeslowski/jsupla/server/entities/responses/registerdevice/BadCredentialsRegisterDeviceResponse.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/entities/responses/registerdevice/BadCredentialsRegisterDeviceResponse.java
@@ -3,7 +3,8 @@ package pl.grzeslowski.jsupla.server.entities.responses.registerdevice;
 import static pl.grzeslowski.jsupla.protocol.consts.ProtoConsts.SUPLA_RESULTCODE_BAD_CREDENTIALS;
 
 public final class BadCredentialsRegisterDeviceResponse extends ErrorRegisterDeviceResponse {
-    public static final BadCredentialsRegisterDeviceResponse BAD_CREDENTIALS = new BadCredentialsRegisterDeviceResponse();
+    public static final BadCredentialsRegisterDeviceResponse BAD_CREDENTIALS =
+            new BadCredentialsRegisterDeviceResponse();
 
     private BadCredentialsRegisterDeviceResponse() {
         super(SUPLA_RESULTCODE_BAD_CREDENTIALS);
