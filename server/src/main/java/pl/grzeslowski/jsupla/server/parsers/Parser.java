a85105136baa6ee27c202f97343877dae0df122e
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:38:12 +0200

    `Method type name 'Rq' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.	7  Method type name 'Rsp' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.`

--- server/src/main/java/pl/grzeslowski/jsupla/server/parsers/Parser.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/parsers/Parser.java
@@ -3,6 +3,6 @@ package pl.grzeslowski.jsupla.server.parsers;
 import pl.grzeslowski.jsupla.protocol.structs.ds.DeviceServer;
 import pl.grzeslowski.jsupla.server.entities.requests.Request;
 
-public interface Parser<Rq extends Request, DS extends DeviceServer> {
-    Rq parse(DS proto);
+public interface Parser<RequestT extends Request, DeviceServerT extends DeviceServer> {
+    RequestT parse(DeviceServerT proto);
 }
