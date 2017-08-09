686624c9c74972d485cadbce7cbef415a0bced32
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:37:13 +0200

    `Method type name 'Rq' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.	7  Method type name 'Rsp' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.`

--- server/src/main/java/pl/grzeslowski/jsupla/server/listeners/ListenersFactory.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/listeners/ListenersFactory.java
@@ -4,5 +4,5 @@ import pl.grzeslowski.jsupla.server.entities.requests.Request;
 import pl.grzeslowski.jsupla.server.entities.responses.Response;
 
 public interface ListenersFactory {
-    <Rq extends Request, Rsp extends Response> RequestListener<Rq, Rsp> getRequestListener(Rq request);
+    <RequestT extends Request, ResponseT extends Response> RequestListener<RequestT, ResponseT> getRequestListener(RequestT request);
 }
