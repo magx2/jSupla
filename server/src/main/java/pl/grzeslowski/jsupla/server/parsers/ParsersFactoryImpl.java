e3489c82902f54c72a43494a6dd4701062054692
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:40:37 +0200

    `Too long line`

--- server/src/main/java/pl/grzeslowski/jsupla/server/parsers/ParsersFactoryImpl.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/parsers/ParsersFactoryImpl.java
@@ -8,15 +8,18 @@ import pl.grzeslowski.jsupla.server.entities.requests.Request;
 import static java.lang.String.format;
 
 public class ParsersFactoryImpl implements ParsersFactory {
-    private final Parser<DeviceRegisterRequestB, SuplaRegisterDeviceB> deviceRegisterRequestBParser = new DeviceRegisterRequestBParser();
+    private final Parser<DeviceRegisterRequestB, SuplaRegisterDeviceB> deviceRegisterRequestBParser =
+            new DeviceRegisterRequestBParser();
 
     @SuppressWarnings("unchecked")
     @Override
-    public <DeviceServerT extends DeviceServer> Parser<? extends Request, DeviceServerT> getParser(DeviceServerT deviceServer) {
+    public <DeviceServerT extends DeviceServer> Parser<? extends Request, DeviceServerT> getParser(
+            DeviceServerT deviceServer) {
         if (deviceServer instanceof SuplaRegisterDeviceB) {
             return (Parser<? extends Request, DeviceServerT>) deviceRegisterRequestBParser;
         }
 
-        throw new IllegalArgumentException(format("Don't know which parser suits for DeviceServer class %s!", deviceServer.getClass().getSimpleName()));
+        throw new IllegalArgumentException(format("Don't know which parser suits for DeviceServer class %s!",
+                deviceServer.getClass().getSimpleName()));
     }
 }
