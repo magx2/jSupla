2925f28be71ead9a45e4e09493fb4d70d3715405
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:39:37 +0200

    `Method type name 'Rq' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.	7  Method type name 'Rsp' must match pattern '(^[A-Z][0-9]?)$|([A-Z][a-zA-Z0-9]*[T]$)'.`

--- server/src/main/java/pl/grzeslowski/jsupla/server/serializers/Serializer.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/serializers/Serializer.java
@@ -3,6 +3,6 @@ package pl.grzeslowski.jsupla.server.serializers;
 import pl.grzeslowski.jsupla.protocol.structs.sd.ServerDevice;
 import pl.grzeslowski.jsupla.server.entities.responses.Response;
 
-public interface Serializer<Rsp extends Response, SD extends ServerDevice> {
-    SD serialize(Rsp entity);
+public interface Serializer<ResponseT extends Response, ServerDeviceT extends ServerDevice> {
+    ServerDeviceT serialize(ResponseT response);
 }
