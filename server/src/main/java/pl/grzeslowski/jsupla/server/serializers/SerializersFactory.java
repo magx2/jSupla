3403bd503d8eae4cafb47275ef9c4d7cb830ba5f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:41:23 +0200

    `Too long line`

--- server/src/main/java/pl/grzeslowski/jsupla/server/serializers/SerializersFactory.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/serializers/SerializersFactory.java
@@ -4,5 +4,6 @@ import pl.grzeslowski.jsupla.protocol.structs.sd.ServerDevice;
 import pl.grzeslowski.jsupla.server.entities.responses.Response;
 
 public interface SerializersFactory {
-    <ResponseT extends Response> Serializer<ResponseT, ? extends ServerDevice> getSerializerForResponse(ResponseT response);
+    <ResponseT extends Response> Serializer<ResponseT, ? extends ServerDevice> getSerializerForResponse(
+            ResponseT response);
 }
