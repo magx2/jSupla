ade3e33cec89cc476b33552d784940d236688810
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:17:42 +0200

    Java doc

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/structs/sd/ServerDevice.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/structs/sd/ServerDevice.java
@@ -4,7 +4,7 @@ import pl.grzeslowski.jsupla.protocol.Proto;
 import pl.grzeslowski.jsupla.protocol.calltypes.ServerDeviceCallType;
 
 /**
- * Structs send from server to device (client)
+ * Structs send from server to device (client).
  */
 public interface ServerDevice extends Proto {
     ServerDeviceCallType callType();
