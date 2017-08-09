64a8f68dfbf77a5ef5ac5a56375e8297498c5b83
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 00:08:53 +0200

    Removing old java doc

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/structs/ds/DeviceServer.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/structs/ds/DeviceServer.java
@@ -3,9 +3,6 @@ package pl.grzeslowski.jsupla.protocol.structs.ds;
 import pl.grzeslowski.jsupla.protocol.Proto;
 import pl.grzeslowski.jsupla.protocol.calltypes.DeviceServerCallType;
 
-/**
- * Structs send from device (client) to server
- */
 public interface DeviceServer extends Proto {
     DeviceServerCallType callType();
 }
