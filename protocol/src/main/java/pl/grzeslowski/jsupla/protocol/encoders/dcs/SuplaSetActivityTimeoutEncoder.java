c2a378ceabbb6d06280d492e1f14e792bbe2cadf
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 00:34:32 +0200

    SuplaSetActivityTimeoutEncoder.java

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/encoders/dcs/SuplaSetActivityTimeoutEncoder.java
@@ -0,0 +1,16 @@
+package pl.grzeslowski.jsupla.protocol.encoders.dcs;
+
+import pl.grzeslowski.jsupla.protocol.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.structs.dcs.SuplaSetActivityTimeout;
+
+public class SuplaSetActivityTimeoutEncoder implements DeviceClientServerEncoder<SuplaSetActivityTimeout> {
+    @Override
+    public byte[] encode(SuplaSetActivityTimeout proto) {
+        byte[] data = new byte[proto.size()];
+        int offset = 0;
+
+        PrimitiveEncoder.writeUnsignedByte(proto.activityTimeout, data, offset);
+
+        return data;
+    }
+}
