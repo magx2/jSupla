5b80dbf8d2c8206b9239fd074109b53309328ec7
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 19:02:01 +0200

    Removing call types from ProtoConsts.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/consts/ProtoConsts.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/consts/ProtoConsts.java
@@ -27,35 +27,6 @@ public final class ProtoConsts {
     public static final int SUPLA_URL_PATH_MAXSIZE = 101;
     public static final int SUPLA_SERVER_NAME_MAXSIZE = 65;
 
-    public static final int SUPLA_DCS_CALL_GETVERSION = 10;
-    public static final int SUPLA_SDC_CALL_GETVERSION_RESULT = 20;
-    public static final int SUPLA_SDC_CALL_VERSIONERROR = 30;
-    public static final int SUPLA_DCS_CALL_PING_SERVER = 40;
-    public static final int SUPLA_SDC_CALL_PING_SERVER_RESULT = 50;
-    public static final int SUPLA_DS_CALL_REGISTER_DEVICE = 60;
-    public static final int SUPLA_DS_CALL_REGISTER_DEVICE_B = 65; // ver. >= 2
-    public static final int SUPLA_DS_CALL_REGISTER_DEVICE_C = 67; // ver. >= 6
-    public static final int SUPLA_SD_CALL_REGISTER_DEVICE_RESULT = 70;
-    public static final int SUPLA_CS_CALL_REGISTER_CLIENT = 80;
-    public static final int SUPLA_CS_CALL_REGISTER_CLIENT_B = 85; // ver. >= 6
-    public static final int SUPLA_SC_CALL_REGISTER_CLIENT_RESULT = 90;
-    public static final int SUPLA_DS_CALL_DEVICE_CHANNEL_VALUE_CHANGED = 100;
-    public static final int SUPLA_SD_CALL_CHANNEL_SET_VALUE = 110;
-    public static final int SUPLA_DS_CALL_CHANNEL_SET_VALUE_RESULT = 120;
-    public static final int SUPLA_SC_CALL_LOCATION_UPDATE = 130;
-    public static final int SUPLA_SC_CALL_LOCATIONPACK_UPDATE = 140;
-    public static final int SUPLA_SC_CALL_CHANNEL_UPDATE = 150;
-    public static final int SUPLA_SC_CALL_CHANNELPACK_UPDATE = 160;
-    public static final int SUPLA_SC_CALL_CHANNEL_VALUE_UPDATE = 170;
-    public static final int SUPLA_CS_CALL_GET_NEXT = 180;
-    public static final int SUPLA_SC_CALL_EVENT = 190;
-    public static final int SUPLA_CS_CALL_CHANNEL_SET_VALUE = 200;
-    public static final int SUPLA_CS_CALL_CHANNEL_SET_VALUE_B = 205; // ver. >= 3
-    public static final int SUPLA_DCS_CALL_SET_ACTIVITY_TIMEOUT = 210; // ver. >= 2
-    public static final int SUPLA_SDC_CALL_SET_ACTIVITY_TIMEOUT_RESULT = 220; // ver. >= 2
-    public static final int SUPLA_DS_CALL_GET_FIRMWARE_UPDATE_URL = 300; // ver. >= 5
-    public static final int SUPLA_SD_CALL_GET_FIRMWARE_UPDATE_URL_RESULT = 310; // ver. >= 5
-
     public static final int SUPLA_RESULT_DATA_TOO_LARGE = -4;
     public static final int SUPLA_RESULT_BUFFER_OVERFLOW = -3;
     public static final int SUPLA_RESULT_DATA_ERROR = -2;
