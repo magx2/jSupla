95d77d3759eb40085a74403c3222b90ad64d6254
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 00:10:10 +0200

    SuplaChannelPackEncoder.java

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/encoders/sc/SuplaChannelPackEncoder.java
@@ -0,0 +1,30 @@
+package pl.grzeslowski.jsupla.protocol.encoders.sc;
+
+import pl.grzeslowski.jsupla.protocol.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.structs.sc.SuplaChannel;
+import pl.grzeslowski.jsupla.protocol.structs.sc.SuplaChannelPack;
+
+import static java.util.Objects.requireNonNull;
+
+public class SuplaChannelPackEncoder implements ServerClientEncoder<SuplaChannelPack> {
+    private final SuplaChannelEncoder channelEncoder;
+
+    public SuplaChannelPackEncoder(SuplaChannelEncoder channelEncoder) {
+        this.channelEncoder = requireNonNull(channelEncoder);
+    }
+
+    @Override
+    public byte[] encode(SuplaChannelPack proto) {
+        byte[] data = new byte[proto.size()];
+        int offset = 0;
+
+        offset += PrimitiveEncoder.writeInteger(proto.count, data, offset);
+        offset += PrimitiveEncoder.writeInteger(proto.totalLeft, data, offset);
+        for (SuplaChannel channel : proto.channels) {
+            final byte[] channelBytes = channelEncoder.encode(channel);
+            offset += PrimitiveEncoder.writeBytes(channelBytes, data, offset);
+        }
+
+        return data;
+    }
+}
