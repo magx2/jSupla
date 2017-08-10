acb27ff632c1cab10b21d351a2197cf32116d6ac
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 01:23:37 +0200

    Adding nettyServer project

--- nettyServer/src/main/java/pl/grzeslowski/jsupla/server/netty/NettyConfig.java
+++ nettyServer/src/main/java/pl/grzeslowski/jsupla/server/netty/NettyConfig.java
@@ -1,14 +1,14 @@
 package pl.grzeslowski.jsupla.server.netty;
 
 
-import static pl.grzeslowski.jsupla.Preconditions.size;
+import pl.grzeslowski.jsupla.Preconditions;
 
 public final class NettyConfig {
     private static final int MAX_PORT_VALUE = 65_535;
     private final int port;
 
     public NettyConfig(int port) {
-        this.port = size(port, 0, MAX_PORT_VALUE);
+        this.port = Preconditions.size(port, 0, MAX_PORT_VALUE);
     }
 
     public int getPort() {
