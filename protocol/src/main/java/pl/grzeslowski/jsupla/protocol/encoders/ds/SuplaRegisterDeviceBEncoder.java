bee87b210cd8e081eaaa3bb7b89398994a0e3e27
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 00:31:14 +0200

    SuplaRegisterDeviceBEncoder.java

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/encoders/ds/SuplaRegisterDeviceBEncoder.java
@@ -0,0 +1,35 @@
+package pl.grzeslowski.jsupla.protocol.encoders.ds;
+
+import pl.grzeslowski.jsupla.protocol.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.structs.ds.SuplaDeviceChannelB;
+import pl.grzeslowski.jsupla.protocol.structs.ds.SuplaRegisterDeviceB;
+
+import static java.util.Objects.requireNonNull;
+
+@Deprecated
+public class SuplaRegisterDeviceBEncoder implements DeviceServerEncoder<SuplaRegisterDeviceB> {
+    private final SuplaDeviceChannelBEncoder deviceChannelBEncoder;
+
+    public SuplaRegisterDeviceBEncoder(SuplaDeviceChannelBEncoder deviceChannelBEncoder) {
+        this.deviceChannelBEncoder = requireNonNull(deviceChannelBEncoder);
+    }
+
+    @Override
+    public byte[] encode(SuplaRegisterDeviceB proto) {
+        byte[] data = new byte[proto.size()];
+        int offset = 0;
+
+        offset += PrimitiveEncoder.writeInteger(proto.locationId, data, offset);
+        offset += PrimitiveEncoder.writeBytes(proto.locationPwd, data, offset);
+        offset += PrimitiveEncoder.writeBytes(proto.guid, data, offset);
+        offset += PrimitiveEncoder.writeBytes(proto.name, data, offset);
+        offset += PrimitiveEncoder.writeBytes(proto.softVer, data, offset);
+        offset += PrimitiveEncoder.writeUnsignedByte(proto.channelCount, data, offset);
+        for (SuplaDeviceChannelB channel : proto.channels) {
+            final byte[] channelBytes = deviceChannelBEncoder.encode(channel);
+            offset += PrimitiveEncoder.writeBytes(channelBytes, data, offset);
+        }
+
+        return data;
+    }
+}
