19d19c938c5ba6b7079629ab08b7fed66fe51edd
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 00:18:34 +0200

    SuplaRegisterClientResultEncoder.java

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/encoders/sc/SuplaRegisterClientResultEncoder.java
@@ -0,0 +1,20 @@
+package pl.grzeslowski.jsupla.protocol.encoders.sc;
+
+import pl.grzeslowski.jsupla.protocol.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.structs.sc.SuplaRegisterClientResult;
+
+public class SuplaRegisterClientResultEncoder implements ServerClientEncoder<SuplaRegisterClientResult> {
+    @SuppressWarnings("UnusedAssignment")
+    @Override
+    public byte[] encode(SuplaRegisterClientResult proto) {
+        byte[] data = new byte[proto.size()];
+        int offset = 0;
+
+        offset += PrimitiveEncoder.writeInteger(proto.resultCode, data, offset);
+        offset += PrimitiveEncoder.writeInteger(proto.clientId, data, offset);
+        offset += PrimitiveEncoder.writeInteger(proto.locationCount, data, offset);
+        offset += PrimitiveEncoder.writeInteger(proto.channelCount, data, offset);
+
+        return data;
+    }
+}
