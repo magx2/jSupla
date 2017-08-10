62e380f635d258e13909070da23ff75cdc6d71e0
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-10 20:58:33 +0200

    Adding call type to Proto

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/Proto.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/Proto.java
@@ -1,8 +1,12 @@
 package pl.grzeslowski.jsupla.protocol;
 
+import pl.grzeslowski.jsupla.protocol.calltypes.CallType;
+
 /**
  * This interface is common for all protocol structures that are send through Supla communication protocol.
  */
 public interface Proto {
     int size();
+
+    CallType callType();
 }
