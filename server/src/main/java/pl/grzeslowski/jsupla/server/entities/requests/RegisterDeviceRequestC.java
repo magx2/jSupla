05bc10d47c689c5776de39143197dfca4c0fb2a9
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-11 21:38:43 +0200

    Fixing compilation problems

--- server/src/main/java/pl/grzeslowski/jsupla/server/entities/requests/RegisterDeviceRequestC.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/entities/requests/RegisterDeviceRequestC.java
@@ -1,11 +1,10 @@
 package pl.grzeslowski.jsupla.server.entities.requests;
 
 import pl.grzeslowski.jsupla.server.entities.Entity;
-import pl.grzeslowski.jsupla.server.entities.misc.DeviceChannelB;
+import pl.grzeslowski.jsupla.server.entities.misc.DeviceChannelsB;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.List;
 
 import static pl.grzeslowski.jsupla.Preconditions.size;
 import static pl.grzeslowski.jsupla.protocol.consts.ProtoConsts.SUPLA_SERVER_NAME_MAXSIZE;
@@ -17,7 +16,7 @@ public class RegisterDeviceRequestC extends RegisterDeviceRequestB {
     private final String serverName;
 
     public RegisterDeviceRequestC(int locationId, String locationPassword, String guid, String name, String softVersion,
-                                  List<? extends DeviceChannelB> channels, String serverName) {
+                                  DeviceChannelsB channels, String serverName) {
         super(locationId, locationPassword, guid, name, softVersion, channels);
         this.serverName = size(serverName, 1, SUPLA_SERVER_NAME_MAXSIZE);
     }
--- server/src/main/java/pl/grzeslowski/jsupla/server/parsers/DeviceRegisterRequestBParser.java
+++ server/src/main/java/pl/grzeslowski/jsupla/server/parsers/DeviceRegisterRequestBParser.java
@@ -1,11 +1,10 @@
 package pl.grzeslowski.jsupla.server.parsers;
 
 import pl.grzeslowski.jsupla.protocol.structs.ds.SuplaRegisterDeviceB;
-import pl.grzeslowski.jsupla.server.entities.misc.DeviceChannelB;
+import pl.grzeslowski.jsupla.server.entities.misc.DeviceChannelsB;
 import pl.grzeslowski.jsupla.server.entities.requests.RegisterDeviceRequestB;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static pl.grzeslowski.jsupla.protocol.decoders.PrimitiveDecoder.parseHexString;
 import static pl.grzeslowski.jsupla.protocol.decoders.PrimitiveDecoder.parseUtf8String;
@@ -18,7 +17,7 @@ public class DeviceRegisterRequestBParser implements Parser<RegisterDeviceReques
         String guid = parseHexString(proto.guid);
         String name = parseUtf8String(proto.name);
         String softVersion = parseUtf8String(proto.softVer);
-        List<? extends DeviceChannelB> channels = new ArrayList<>();// TODO
+        DeviceChannelsB channels = new DeviceChannelsB(new ArrayList<>());// TODO
 
         return new RegisterDeviceRequestB(locationId, locationPassword, guid, name, softVersion, channels);
     }
