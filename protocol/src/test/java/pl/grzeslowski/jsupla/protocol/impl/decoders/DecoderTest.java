3b234f5b85a72c6fbec32dd8e17936b3ae927df3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-17 00:25:58 +0200

    Using lambda

--- protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/decoders/DecoderTest.java
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/decoders/DecoderTest.java
@@ -2,8 +2,6 @@ package pl.grzeslowski.jsupla.protocol.impl.decoders;
 
 import org.junit.Before;
 import org.mockito.Mock;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import pl.grzeslowski.jsupla.protocol.api.decoders.PrimitiveDecoder;
 
 import static org.mockito.BDDMockito.given;
@@ -15,14 +13,11 @@ public class DecoderTest {
 
     @Before
     public void init() {
-        given(primitiveDecoder.copyOfRange(any(), anyInt(), anyInt())).will(new Answer<byte[]>() {
-            @Override
-            public byte[] answer(final InvocationOnMock invocation) throws Throwable {
-                final byte[] original = invocation.getArgumentAt(0, byte[].class);
-                final Integer from = invocation.getArgumentAt(1, Integer.class);
-                final Integer to = invocation.getArgumentAt(2, Integer.class);
-                return PrimitiveDecoderImpl.INSTANCE.copyOfRange(original, from, to);
-            }
+        given(primitiveDecoder.copyOfRange(any(), anyInt(), anyInt())).will(invocation -> {
+            final byte[] original = invocation.getArgumentAt(0, byte[].class);
+            final Integer from = invocation.getArgumentAt(1, Integer.class);
+            final Integer to = invocation.getArgumentAt(2, Integer.class);
+            return PrimitiveDecoderImpl.INSTANCE.copyOfRange(original, from, to);
         });
     }
 }
