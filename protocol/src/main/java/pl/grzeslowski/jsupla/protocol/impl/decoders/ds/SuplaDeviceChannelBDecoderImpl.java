79968d345c19f1e03119523f658ca9920487a49e
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-18 10:46:55 +0200

    SuplaDeviceChannelBDecoderImplTest.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/decoders/ds/SuplaDeviceChannelBDecoderImpl.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/decoders/ds/SuplaDeviceChannelBDecoderImpl.java
@@ -1,5 +1,6 @@
 package pl.grzeslowski.jsupla.protocol.impl.decoders.ds;
 
+import pl.grzeslowski.jsupla.Preconditions;
 import pl.grzeslowski.jsupla.protocol.api.decoders.PrimitiveDecoder;
 import pl.grzeslowski.jsupla.protocol.api.decoders.ds.SuplaDeviceChannelBDecoder;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
@@ -18,6 +19,8 @@ public final class SuplaDeviceChannelBDecoderImpl implements SuplaDeviceChannelB
 
     @Override
     public SuplaDeviceChannelB decode(byte[] bytes, int offset) {
+        Preconditions.checkMinArrayLength(bytes, offset + SuplaDeviceChannelB.SIZE);
+
         final short number = primitiveDecoder.parseUnsignedByte(bytes, offset);
         offset += BYTE_SIZE;
 
