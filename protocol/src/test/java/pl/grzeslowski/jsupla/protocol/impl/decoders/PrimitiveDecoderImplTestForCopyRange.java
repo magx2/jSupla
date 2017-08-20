82681fdb855844676102f214b45d84429eb4ee4e
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-21 00:03:17 +0200

    Tests for copy range

---
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/decoders/PrimitiveDecoderImplTestForCopyRange.java
@@ -0,0 +1,43 @@
+package pl.grzeslowski.jsupla.protocol.impl.decoders;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static pl.grzeslowski.jsupla.protocol.impl.decoders.PrimitiveDecoderImpl.INSTANCE;
+
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class PrimitiveDecoderImplTestForCopyRange {
+
+    @Test
+    public void shouldCopyRange() throws Exception {
+
+        // given
+        final byte[] bytes = new byte[]{9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+        // when
+        final byte[] copyOfRange = INSTANCE.copyOfRange(bytes, 3, 6);
+
+        // then
+        assertThat(copyOfRange).isEqualTo(new byte[]{6, 5, 4});
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeWhenOriginalIsNull() throws Exception {
+        INSTANCE.copyOfRange(null, 1, 2);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldThrowArrayIndexOutOfBoundsExceptionWhenFromIsSmallerThan0() throws Exception {
+        INSTANCE.copyOfRange(new byte[5], -1, 2);
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void shouldThrowArrayIndexOutOfBoundsExceptionWhenFromIsBiggerThanArray() throws Exception {
+        INSTANCE.copyOfRange(new byte[5], 6, 7);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIaeWhenFromIsBiggerThanTo() throws Exception {
+        INSTANCE.copyOfRange(new byte[5], 2, 1);
+    }
+}
