c13ceac097437be79b90d4de58b753558d482eac
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-21 22:31:12 +0200

    SuplaChannelNewValueBEncoderImplTest.java

---
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/EncoderTest.java
@@ -0,0 +1,50 @@
+package pl.grzeslowski.jsupla.protocol.impl.encoders;
+
+import org.junit.Test;
+import org.mockito.Mock;
+import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
+import pl.grzeslowski.jsupla.protocol.api.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.api.types.ProtoWithSize;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyByte;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.BYTE_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.INT_SIZE;
+
+@SuppressWarnings("WeakerAccess")
+public abstract class EncoderTest<ProtoT extends ProtoWithSize> {
+    @Mock protected PrimitiveEncoder primitiveEncoder;
+
+    void givenEncodeEntity() {
+        given(primitiveEncoder.writeByte(anyByte(), any(), anyInt())).willReturn(BYTE_SIZE);
+        given(primitiveEncoder.writeUnsignedByte(anyByte(), any(), anyInt())).willReturn(BYTE_SIZE);
+        given(primitiveEncoder.writeInteger(anyInt(), any(), anyInt())).willReturn(INT_SIZE);
+        given(primitiveEncoder.writeUnsignedInteger(anyInt(), any(), anyInt())).willReturn(INT_SIZE);
+    }
+
+    @Test
+    public final void shouldEncodeEntity() throws Exception {
+
+        // given
+        givenEncodeEntity();
+
+        // when
+        final byte[] encode = getEncoder().encode(getProto());
+
+        // then
+        verifyEncodeEntity(encode);
+        verifyNoMoreInteractions(primitiveEncoder);
+    }
+
+    protected abstract void verifyEncodeEntity(final byte[] encode);
+
+    @Test(expected = NullPointerException.class)
+    public abstract void shouldThrowNpeWhenPrimitiveParserIsNull() throws Exception;
+
+    public abstract Encoder<ProtoT> getEncoder();
+
+    public abstract ProtoT getProto();
+}
---
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/cs/SuplaChannelNewValueBEncoderImplTest.java
@@ -0,0 +1,45 @@
+package pl.grzeslowski.jsupla.protocol.impl.encoders.cs;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
+import pl.grzeslowski.jsupla.protocol.api.structs.cs.SuplaChannelNewValueB;
+import pl.grzeslowski.jsupla.protocol.impl.encoders.EncoderTest;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.INT_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_CHANNELVALUE_SIZE;
+
+@SuppressWarnings("WeakerAccess")
+@RunWith(MockitoJUnitRunner.class)
+public class SuplaChannelNewValueBEncoderImplTest extends EncoderTest<SuplaChannelNewValueB> {
+    @InjectMocks SuplaChannelNewValueBEncoderImpl encoder;
+
+    @Override
+    protected void verifyEncodeEntity(final byte[] encode) {
+        int offset = 0;
+        verify(primitiveEncoder).writeInteger(eq(getProto().channelId), any(byte[].class), eq(offset));
+        offset += INT_SIZE;
+        verify(primitiveEncoder).writeBytes(eq(getProto().value), any(byte[].class), eq(offset));
+    }
+
+    @Override
+    public void shouldThrowNpeWhenPrimitiveParserIsNull() throws Exception {
+        new SuplaChannelNewValueBEncoderImpl(null);
+    }
+
+    @Override
+    public Encoder<SuplaChannelNewValueB> getEncoder() {
+        return encoder;
+    }
+
+    @Override
+    public SuplaChannelNewValueB getProto() {
+        final int channelId = 1;
+        final byte[] value = new byte[SUPLA_CHANNELVALUE_SIZE];
+        return new SuplaChannelNewValueB(channelId, value);
+    }
+}
