128f94beb12f94cd466b05717bd2d2f1f18c3a73
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-21 23:20:02 +0200

    SuplaRegisterDeviceCEncoderImplTest.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/encoders/ds/SuplaRegisterDeviceCEncoderImpl.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/encoders/ds/SuplaRegisterDeviceCEncoderImpl.java
@@ -1,6 +1,7 @@
 package pl.grzeslowski.jsupla.protocol.impl.encoders.ds;
 
 import pl.grzeslowski.jsupla.protocol.api.encoders.PrimitiveEncoder;
+import pl.grzeslowski.jsupla.protocol.api.encoders.ds.SuplaDeviceChannelBEncoder;
 import pl.grzeslowski.jsupla.protocol.api.encoders.ds.SuplaRegisterDeviceCEncoder;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
 import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaRegisterDeviceC;
@@ -9,10 +10,10 @@ import static java.util.Objects.requireNonNull;
 
 public final class SuplaRegisterDeviceCEncoderImpl implements SuplaRegisterDeviceCEncoder {
     private final PrimitiveEncoder primitiveEncoder;
-    private final SuplaDeviceChannelBEncoderImpl deviceChannelBEncoder;
+    private final SuplaDeviceChannelBEncoder deviceChannelBEncoder;
 
     public SuplaRegisterDeviceCEncoderImpl(PrimitiveEncoder primitiveEncoder,
-                                           SuplaDeviceChannelBEncoderImpl deviceChannelBEncoder) {
+                                           SuplaDeviceChannelBEncoder deviceChannelBEncoder) {
         this.primitiveEncoder = requireNonNull(primitiveEncoder);
         this.deviceChannelBEncoder = requireNonNull(deviceChannelBEncoder);
     }
@@ -28,6 +29,7 @@ public final class SuplaRegisterDeviceCEncoderImpl implements SuplaRegisterDevic
         offset += primitiveEncoder.writeBytes(proto.name, data, offset);
         offset += primitiveEncoder.writeBytes(proto.softVer, data, offset);
         offset += primitiveEncoder.writeBytes(proto.serverName, data, offset);
+        offset += primitiveEncoder.writeUnsignedByte(proto.channelCount, data, offset);
         for (SuplaDeviceChannelB channel : proto.channels) {
             final byte[] channelBytes = deviceChannelBEncoder.encode(channel);
             offset += primitiveEncoder.writeBytes(channelBytes, data, offset);
---
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/ds/SuplaRegisterDeviceCEncoderImplTest.java
@@ -0,0 +1,101 @@
+package pl.grzeslowski.jsupla.protocol.impl.encoders.ds;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
+import pl.grzeslowski.jsupla.protocol.api.encoders.ds.SuplaDeviceChannelBEncoder;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaRegisterDeviceC;
+import pl.grzeslowski.jsupla.protocol.impl.encoders.EncoderTest;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.BYTE_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.INT_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_CHANNELVALUE_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_DEVICE_NAME_MAXSIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_GUID_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_LOCATION_PWD_MAXSIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_SERVER_NAME_MAXSIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_SOFTVER_MAXSIZE;
+
+@SuppressWarnings("WeakerAccess")
+@RunWith(MockitoJUnitRunner.class)
+public class SuplaRegisterDeviceCEncoderImplTest extends EncoderTest<SuplaRegisterDeviceC> {
+    @InjectMocks SuplaRegisterDeviceCEncoderImpl encoder;
+    @Mock SuplaDeviceChannelBEncoder deviceChannelBEncoder;
+
+    @Override
+    protected void givenEncodeEntity() {
+        super.givenEncodeEntity();
+        given(deviceChannelBEncoder.encode(any(SuplaDeviceChannelB.class))).willAnswer(
+                invocationOnMock -> new byte[SuplaDeviceChannelB.SIZE]
+        );
+    }
+
+    @Override
+    protected void verifyEncodeEntity(final byte[] encode, final SuplaRegisterDeviceC proto) {
+        int offset = 0;
+
+        verify(primitiveEncoder).writeInteger(proto.locationId, bytesToWriteInto(), offset);
+        offset += INT_SIZE;
+        verify(primitiveEncoder).writeBytes(proto.locationPwd, bytesToWriteInto(), offset);
+        offset += SUPLA_LOCATION_PWD_MAXSIZE;
+        verify(primitiveEncoder).writeBytes(proto.guid, bytesToWriteInto(), offset);
+        offset += SUPLA_GUID_SIZE;
+        verify(primitiveEncoder).writeBytes(proto.name, bytesToWriteInto(), offset);
+        offset += SUPLA_DEVICE_NAME_MAXSIZE;
+        verify(primitiveEncoder).writeBytes(proto.softVer, bytesToWriteInto(), offset);
+        offset += SUPLA_SOFTVER_MAXSIZE;
+        verify(primitiveEncoder).writeBytes(proto.serverName, bytesToWriteInto(), offset);
+        offset += SUPLA_SERVER_NAME_MAXSIZE;
+        verify(primitiveEncoder).writeUnsignedByte(proto.channelCount, bytesToWriteInto(), offset);
+        offset += BYTE_SIZE;
+        for (SuplaDeviceChannelB channel : proto.channels) {
+            verify(deviceChannelBEncoder).encode(channel);
+            verify(primitiveEncoder).writeBytes(new byte[SuplaDeviceChannelB.SIZE], bytesToWriteInto(), offset);
+            offset += SuplaDeviceChannelB.SIZE;
+        }
+    }
+
+    @Override
+    public void shouldThrowNpeWhenPrimitiveParserIsNull() throws Exception {
+        new SuplaRegisterDeviceCEncoderImpl(null, deviceChannelBEncoder);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNpeWhenSuplaDeviceChannelBEncoderIsNull() throws Exception {
+        new SuplaRegisterDeviceCEncoderImpl(primitiveEncoder, null);
+    }
+
+    @Override
+    public Encoder<SuplaRegisterDeviceC> getEncoder() {
+        return encoder;
+    }
+
+    @Override
+    public SuplaRegisterDeviceC getProto() {
+        final short channelCount = (short) 10;
+        final SuplaDeviceChannelB[] channels = new SuplaDeviceChannelB[channelCount];
+        for (int i = 0; i < channelCount; i++) {
+            // @formatter:off
+            channels[i] = new SuplaDeviceChannelB(
+                    (short) (i * 7), i * 8, i * 5, i * 6, new byte[SUPLA_CHANNELVALUE_SIZE]);
+            // @formatter:on
+        }
+        return new SuplaRegisterDeviceC(
+                                               1,
+                                               new byte[SUPLA_LOCATION_PWD_MAXSIZE],
+                                               new byte[SUPLA_GUID_SIZE],
+                                               new byte[SUPLA_DEVICE_NAME_MAXSIZE],
+                                               new byte[SUPLA_SOFTVER_MAXSIZE],
+                                               new byte[SUPLA_SERVER_NAME_MAXSIZE],
+                                               channelCount,
+                                               channels
+        );
+    }
+}
