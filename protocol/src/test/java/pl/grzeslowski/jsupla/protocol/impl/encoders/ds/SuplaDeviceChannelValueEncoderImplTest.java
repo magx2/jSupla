990d5747f01cdb464d8e891e138bac74d051f6f5
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-21 23:29:58 +0200

    SuplaDeviceChannelValueEncoderImplTest.java

---
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/ds/SuplaDeviceChannelValueEncoderImplTest.java
@@ -0,0 +1,42 @@
+package pl.grzeslowski.jsupla.protocol.impl.encoders.ds;
+
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelValue;
+import pl.grzeslowski.jsupla.protocol.impl.encoders.EncoderTest;
+
+import static org.mockito.Mockito.verify;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.BYTE_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_CHANNELVALUE_SIZE;
+
+@SuppressWarnings("WeakerAccess")
+@RunWith(MockitoJUnitRunner.class)
+public class SuplaDeviceChannelValueEncoderImplTest extends EncoderTest<SuplaDeviceChannelValue> {
+    @InjectMocks SuplaDeviceChannelValueEncoderImpl encoder;
+
+    @Override
+    protected void verifyEncodeEntity(final byte[] encode, final SuplaDeviceChannelValue proto) {
+        int offset = 0;
+
+        verify(primitiveEncoder).writeUnsignedByte(proto.channelNumber, bytesToWriteInto(), offset);
+        offset += BYTE_SIZE;
+        verify(primitiveEncoder).writeBytes(proto.value, bytesToWriteInto(), offset);
+    }
+
+    @Override
+    public void shouldThrowNpeWhenPrimitiveParserIsNull() throws Exception {
+        new SuplaDeviceChannelValueEncoderImpl(null);
+    }
+
+    @Override
+    public Encoder<SuplaDeviceChannelValue> getEncoder() {
+        return encoder;
+    }
+
+    @Override
+    public SuplaDeviceChannelValue getProto() {
+        return new SuplaDeviceChannelValue((short) 5, new byte[SUPLA_CHANNELVALUE_SIZE]);
+    }
+}
