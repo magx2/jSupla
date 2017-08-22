88b6e93d5861be14dcbbfbb77a5550c95ffa3d86
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-22 21:32:04 +0200

    SuplaGetVersionResultEncoderImplTest.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sdc/SuplaGetVersionResult.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sdc/SuplaGetVersionResult.java
@@ -4,8 +4,8 @@ import pl.grzeslowski.jsupla.protocol.api.calltypes.ServerDeviceClientCallType;
 
 import java.util.Arrays;
 
-import static java.lang.String.format;
 import static pl.grzeslowski.jsupla.Preconditions.checkArrayLength;
+import static pl.grzeslowski.jsupla.Preconditions.max;
 import static pl.grzeslowski.jsupla.protocol.api.calltypes.ServerDeviceClientCallType.SUPLA_SDC_CALL_GETVERSION_RESULT;
 import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.BYTE_SIZE;
 import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_SOFTVER_MAXSIZE;
@@ -22,14 +22,9 @@ public final class SuplaGetVersionResult implements ServerDeviceClient {
     public final byte[] softVer;
 
     public SuplaGetVersionResult(short protoVersionMin, short protoVersion, byte[] softVer) {
-        this.protoVersionMin = protoVersionMin;
+        this.protoVersionMin = max(protoVersionMin, protoVersion);
         this.protoVersion = protoVersion;
         this.softVer = checkArrayLength(softVer, SUPLA_SOFTVER_MAXSIZE);
-        if (protoVersionMin > protoVersion) {
-            throw new IllegalArgumentException(
-                    format("protoVersionMin (%s) need to be smaller than protoVersion (%s)!",
-                            protoVersionMin, protoVersion));
-        }
     }
 
     @Override
