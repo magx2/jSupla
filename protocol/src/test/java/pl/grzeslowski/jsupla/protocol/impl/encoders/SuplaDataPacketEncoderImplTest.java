f1ffd6bc00a26616489b1d68397717d402dc7e24
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-22 21:51:58 +0200

    SuplaDataPacketEncoderImplTest.java

--- protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/SuplaDataPacketEncoderImplTest.java
+++ protocol/src/test/java/pl/grzeslowski/jsupla/protocol/impl/encoders/SuplaDataPacketEncoderImplTest.java
@@ -1,17 +1,22 @@
-package pl.grzeslowski.jsupla.protocol.api.encoders;
+package pl.grzeslowski.jsupla.protocol.impl.encoders;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
 import pl.grzeslowski.jsupla.protocol.api.structs.SuplaDataPacket;
-import pl.grzeslowski.jsupla.protocol.impl.encoders.PrimitiveEncoderImpl;
-import pl.grzeslowski.jsupla.protocol.impl.encoders.SuplaDataPacketEncoderImpl;
 
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
 import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.BYTE_SIZE;
 import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.INT_SIZE;
 
-public class ToSuplaDataPacketEncoderTest {
+@RunWith(MockitoJUnitRunner.class)
+public class SuplaDataPacketEncoderImplTest extends EncoderTest<SuplaDataPacket> {
+    @InjectMocks SuplaDataPacketEncoderImpl mockedEncoder;
     private final SuplaDataPacketEncoderImpl encoder = new SuplaDataPacketEncoderImpl(PrimitiveEncoderImpl.INSTANCE);
 
     @Test
@@ -63,5 +68,35 @@ public class ToSuplaDataPacketEncoderTest {
         final byte[] bytes = new byte[size];
         random.nextBytes(bytes);
         return bytes;
+    }
+
+    @Override
+    protected void verifyEncodeEntity(final byte[] encode, final SuplaDataPacket proto) {
+        int offset = 0;
+
+        verify(primitiveEncoder).writeUnsignedByte(proto.version, bytesToWriteInto(), offset);
+        offset += BYTE_SIZE;
+        verify(primitiveEncoder).writeUnsignedInteger(proto.rrId, bytesToWriteInto(), offset);
+        offset += INT_SIZE;
+        verify(primitiveEncoder).writeUnsignedInteger(proto.callType, bytesToWriteInto(), offset);
+        offset += INT_SIZE;
+        verify(primitiveEncoder).writeUnsignedInteger(proto.dataSize, bytesToWriteInto(), offset);
+        offset += INT_SIZE;
+        verify(primitiveEncoder).writeBytes(proto.data, bytesToWriteInto(), offset);
+    }
+
+    @Override
+    public void shouldThrowNpeWhenPrimitiveParserIsNull() throws Exception {
+        new SuplaDataPacketEncoderImpl(null);
+    }
+
+    @Override
+    public Encoder<SuplaDataPacket> getEncoder() {
+        return mockedEncoder;
+    }
+
+    @Override
+    public SuplaDataPacket getProto() {
+        return new SuplaDataPacket((short) 1, 2, 3, 4, new byte[4]);
     }
 }
