edd2c8834bfefb397ad250f9afffd0af619db8ce
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-25 11:36:57 +0200

    Adding decoder interface

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/decoders/Decoder.java
@@ -0,0 +1,10 @@
+package pl.grzeslowski.jsupla.protocol.api.decoders;
+
+public interface Decoder<T> {
+    T decode(byte[] bytes, int offset);
+
+    default T decode(byte[] bytes) {
+        return decode(bytes, 0);
+    }
+
+}
--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/decoders/ProtoWithSizeDecoder.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/decoders/ProtoWithSizeDecoder.java
@@ -3,9 +3,7 @@ package pl.grzeslowski.jsupla.protocol.api.decoders;
 import pl.grzeslowski.jsupla.protocol.api.structs.SuplaDataPacket;
 import pl.grzeslowski.jsupla.protocol.api.types.ProtoWithSize;
 
-public interface ProtoWithSizeDecoder<T extends ProtoWithSize> {
-    T decode(byte[] bytes, int offset);
-
+public interface ProtoWithSizeDecoder<T extends ProtoWithSize> extends Decoder<T> {
     default T decode(SuplaDataPacket dataPacket) {
         return decode(dataPacket.data, 0);
     }
