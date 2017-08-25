aa68730d7c1b19eb9a33a65765e5045ac5d565f7
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-25 15:04:19 +0200

    doSwitch returning value

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channelvalues/ChannelValueSwitch.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channelvalues/ChannelValueSwitch.java
@@ -3,30 +3,30 @@ package pl.grzeslowski.jsupla.protocol.api.channelvalues;
 import static java.lang.String.format;
 import static java.util.Objects.requireNonNull;
 
-public final class ChannelValueSwitch {
-    private final Callback callback;
+public final class ChannelValueSwitch<T> {
+    private final Callback<T> callback;
 
-    public ChannelValueSwitch(final Callback callback) {
+    public ChannelValueSwitch(final Callback<T> callback) {
         this.callback = requireNonNull(callback);
     }
 
-    public void doSwitch(ChannelValue channelValue) {
+    public T doSwitch(ChannelValue channelValue) {
         if (channelValue instanceof DecimalValue) {
-            callback.onDecimalValue((DecimalValue) channelValue);
+            return callback.onDecimalValue((DecimalValue) channelValue);
         } else if (channelValue instanceof OnOff) {
-            callback.onOnOff((OnOff) channelValue);
+            return callback.onOnOff((OnOff) channelValue);
         } else if (channelValue instanceof OpenClose) {
-            callback.onOpenClose((OpenClose) channelValue);
+            return callback.onOpenClose((OpenClose) channelValue);
         } else if (channelValue instanceof PercentValue) {
-            callback.onPercentValue((PercentValue) channelValue);
+            return callback.onPercentValue((PercentValue) channelValue);
         } else if (channelValue instanceof RgbValue) {
-            callback.onRgbValue((RgbValue) channelValue);
+            return callback.onRgbValue((RgbValue) channelValue);
         } else if (channelValue instanceof StoppableOpenClose) {
-            callback.onStoppableOpenClose((StoppableOpenClose) channelValue);
+            return callback.onStoppableOpenClose((StoppableOpenClose) channelValue);
         } else if (channelValue instanceof TemperatureAndHumidityValue) {
-            callback.onTemperatureAndHumidityValue((TemperatureAndHumidityValue) channelValue);
+            return callback.onTemperatureAndHumidityValue((TemperatureAndHumidityValue) channelValue);
         } else if (channelValue instanceof UnknownValue) {
-            callback.onUnknownValue((UnknownValue) channelValue);
+            return callback.onUnknownValue((UnknownValue) channelValue);
         } else {
             throw new IllegalArgumentException(format("Don't know where to dispatch channel value with class %s! " +
                                                               "This should NEVER occur on production!",
@@ -34,21 +34,22 @@ public final class ChannelValueSwitch {
         }
     }
 
-    public interface Callback {
-        void onDecimalValue(DecimalValue decimalValue);
+    @SuppressWarnings("UnusedReturnValue")
+    public interface Callback<T> {
+        T onDecimalValue(DecimalValue decimalValue);
 
-        void onOnOff(OnOff onOff);
+        T onOnOff(OnOff onOff);
 
-        void onOpenClose(OpenClose openClose);
+        T onOpenClose(OpenClose openClose);
 
-        void onPercentValue(PercentValue percentValue);
+        T onPercentValue(PercentValue percentValue);
 
-        void onRgbValue(RgbValue rgbValue);
+        T onRgbValue(RgbValue rgbValue);
 
-        void onStoppableOpenClose(StoppableOpenClose stoppableOpenClose);
+        T onStoppableOpenClose(StoppableOpenClose stoppableOpenClose);
 
-        void onTemperatureAndHumidityValue(TemperatureAndHumidityValue temperatureAndHumidityValue);
+        T onTemperatureAndHumidityValue(TemperatureAndHumidityValue temperatureAndHumidityValue);
 
-        void onUnknownValue(UnknownValue unknownValue);
+        T onUnknownValue(UnknownValue unknownValue);
     }
 }
