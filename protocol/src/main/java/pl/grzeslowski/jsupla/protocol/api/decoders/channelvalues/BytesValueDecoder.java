8802d328b7c2e88ed4df2c4640ec5621e6a42db2
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-25 11:44:05 +0200

    Adding BytesValue.javae

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/decoders/channelvalues/BytesValueDecoder.java
@@ -0,0 +1,6 @@
+package pl.grzeslowski.jsupla.protocol.api.decoders.channelvalues;
+
+import pl.grzeslowski.jsupla.protocol.api.channelvalues.BytesValue;
+
+public interface BytesValueDecoder extends ChannelValueDecoder<BytesValue> {
+}
