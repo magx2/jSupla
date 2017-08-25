3b8c2c986d02426b1902d186619101e7e39a65c3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-25 14:57:29 +0200

    Implementing methods

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/decoders/channeltypes/ChannelTypeDecoderImpl.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/impl/decoders/channeltypes/ChannelTypeDecoderImpl.java
@@ -2,10 +2,17 @@ package pl.grzeslowski.jsupla.protocol.impl.decoders.channeltypes;
 
 import pl.grzeslowski.jsupla.protocol.api.channeltypes.ds.ChannelTypeDecoder;
 import pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannel;
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
 
 public class ChannelTypeDecoderImpl implements ChannelTypeDecoder {
     @Override
-    public ChannelValue decode(final byte[] bytes, final int offset) {
-        return null;
+    public ChannelValue decode(final SuplaDeviceChannel channel) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ChannelValue decode(final SuplaDeviceChannelB channel) {
+        throw new UnsupportedOperationException();
     }
 }
