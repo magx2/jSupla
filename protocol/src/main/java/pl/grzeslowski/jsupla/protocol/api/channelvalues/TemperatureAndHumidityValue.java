c77d61e8aedaa061bd64bf27bd419a7e929b2bcc
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-08-27 20:36:56 +0200

    ThermometerTypeChannelDecoderImpl.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channelvalues/TemperatureAndHumidityValue.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channelvalues/TemperatureAndHumidityValue.java
@@ -1,15 +1,45 @@
 package pl.grzeslowski.jsupla.protocol.api.channelvalues;
 
+import java.math.BigDecimal;
+
 import static java.util.Objects.requireNonNull;
 
 @SuppressWarnings("WeakerAccess")
 public final class TemperatureAndHumidityValue implements ChannelValue {
-    public final DecimalValue temperature;
-    public final DecimalValue humidity;
+    public final BigDecimal temperature;
+    public final BigDecimal humidity;
 
-    public TemperatureAndHumidityValue(final DecimalValue temperature, final DecimalValue humidity) {
+    public TemperatureAndHumidityValue(final BigDecimal temperature, final BigDecimal humidity) {
         this.temperature = requireNonNull(temperature);
         this.humidity = requireNonNull(humidity);
+    }
+
+    public TemperatureAndHumidityValue(final int temperature, final int humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final long temperature, final long humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final double temperature, final double humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final float temperature, final float humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final byte temperature, final byte humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final short temperature, final short humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
+    }
+
+    public TemperatureAndHumidityValue(final String temperature, final String humidity) {
+        this(new BigDecimal(temperature), new BigDecimal(humidity));
     }
 
     @Override
