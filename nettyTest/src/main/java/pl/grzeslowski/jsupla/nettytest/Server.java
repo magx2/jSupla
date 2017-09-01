327fcb60a414df61f499492998ff51dfb45bb56a
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-01 18:31:07 +0200

    Using lambdas

--- nettyTest/src/main/java/pl/grzeslowski/jsupla/nettytest/Server.java
+++ nettyTest/src/main/java/pl/grzeslowski/jsupla/nettytest/Server.java
@@ -7,13 +7,11 @@ import pl.grzeslowski.jsupla.protocol.impl.decoders.DecoderFactoryImpl;
 import pl.grzeslowski.jsupla.protocol.impl.decoders.PrimitiveDecoderImpl;
 import pl.grzeslowski.jsupla.protocol.impl.encoders.EncoderFactoryImpl;
 import pl.grzeslowski.jsupla.server.SuplaStreamImpl;
-import pl.grzeslowski.jsupla.server.entities.requests.Request;
 import pl.grzeslowski.jsupla.server.entities.requests.ds.ActivityTimeoutRequest;
 import pl.grzeslowski.jsupla.server.entities.requests.ds.RegisterDeviceRequest;
 import pl.grzeslowski.jsupla.server.entities.responses.ActivityTimeoutResponse;
 import pl.grzeslowski.jsupla.server.entities.responses.registerdevice.OkRegisterDeviceResponse;
 import pl.grzeslowski.jsupla.server.ents.SuplaDataPacketFactoryImpl;
-import pl.grzeslowski.jsupla.server.ents.channelandpublisher.ChannelAndRequestFlux;
 import pl.grzeslowski.jsupla.server.ents.channelandpublisher.mappers.FromServerProtoToRequest;
 import pl.grzeslowski.jsupla.server.ents.channelandpublisher.mappers.FromServerProtoToRequestImpl;
 import pl.grzeslowski.jsupla.server.ents.channelandpublisher.mappers.SuplaDataPacketToFromServerProto;
@@ -26,7 +24,6 @@ import pl.grzeslowski.jsupla.server.serializers.SerializersFactory;
 import pl.grzeslowski.jsupla.server.serializers.SerializersFactoryImpl;
 
 import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 public class Server {
     private final Logger logger = LoggerFactory.getLogger(Server.class);
@@ -56,25 +53,17 @@ public class Server {
         final SuplaStreamImpl suplaStream =
                 new SuplaStreamImpl(() -> new NettyServer(new NettyConfig(2016)), x, y);
 
-        suplaStream.fullStream().log().subscribe(new Consumer<ChannelAndRequestFlux>() {
-            @Override
-            public void accept(final ChannelAndRequestFlux in) {
-                in.getFlux().log().subscribe(new Consumer<Request>() {
-                    @Override
-                    public void accept(final Request request) {
-                        if (request instanceof RegisterDeviceRequest) {
-                            logger.info("Sending register OK");
-                            in.getChannel().write(new OkRegisterDeviceResponse(200, 5, 1));
-                        } else if (request instanceof ActivityTimeoutRequest) {
-                            final ActivityTimeoutRequest a = (ActivityTimeoutRequest) request;
-                            in.getChannel().write(new ActivityTimeoutResponse(a.getActivityTimeout(), 1, 255));
-                        } else {
-                            logger.warn("Doesn't know this type of request {}", request.getClass().getSimpleName());
-                        }
-                    }
-                });
+        suplaStream.fullStream().log().subscribe(in -> in.getFlux().log().subscribe(request -> {
+            if (request instanceof RegisterDeviceRequest) {
+                logger.info("Sending register OK");
+                in.getChannel().write(new OkRegisterDeviceResponse(200, 5, 1));
+            } else if (request instanceof ActivityTimeoutRequest) {
+                final ActivityTimeoutRequest a = (ActivityTimeoutRequest) request;
+                in.getChannel().write(new ActivityTimeoutResponse(a.getActivityTimeout(), 1, 255));
+            } else {
+                logger.warn("Doesn't know this type of request {}", request.getClass().getSimpleName());
             }
-        });
+        }));
 
         TimeUnit.MINUTES.sleep(10);
         logger.warn("End of sleep; closing server");
