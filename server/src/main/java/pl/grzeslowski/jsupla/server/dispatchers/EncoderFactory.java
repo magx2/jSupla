74a88d3e29418cf8cf161d58b1b39aedbc2cd287
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-01 11:50:07 +0200

    Removing dispatchers package

--- server/src/main/java/pl/grzeslowski/jsupla/server/dispatchers/EncoderFactory.java
+++
@@ -1,8 +0,0 @@
-package pl.grzeslowski.jsupla.server.dispatchers;
-
-import pl.grzeslowski.jsupla.protocol.api.encoders.Encoder;
-import pl.grzeslowski.jsupla.protocol.api.types.ProtoWithSize;
-
-public interface EncoderFactory {
-    <T extends ProtoWithSize> Encoder<T> getEncoderForServerDevice(T t);
-}
--- server/src/main/java/pl/grzeslowski/jsupla/server/dispatchers/SuplaDataPacketDispatcher.java
+++
@@ -1,9 +0,0 @@
-package pl.grzeslowski.jsupla.server.dispatchers;
-
-import pl.grzeslowski.jsupla.protocol.api.structs.SuplaDataPacket;
-
-import java.util.Optional;
-
-public interface SuplaDataPacketDispatcher {
-    Optional<SuplaDataPacket> dispatch(SuplaDataPacket dataPacket);
-}
