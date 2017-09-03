4f4b192dbe5b1c356d2c18e4c621125884d3471f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-04 00:01:02 +0200

    @SuppressWarnings("WeakerAccess")

--- nettyServer/src/main/java/pl/grzeslowski/jsupla/server/netty/NettyConfig.java
+++ nettyServer/src/main/java/pl/grzeslowski/jsupla/server/netty/NettyConfig.java
@@ -3,6 +3,7 @@ package pl.grzeslowski.jsupla.server.netty;
 import io.netty.handler.ssl.SslContext;
 import pl.grzeslowski.jsupla.Preconditions;
 
+@SuppressWarnings("WeakerAccess")
 public class NettyConfig {
     private static final int MAX_PORT_VALUE = 65_535;
     private final int port;
