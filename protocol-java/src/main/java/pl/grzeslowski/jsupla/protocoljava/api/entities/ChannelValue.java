f7b2159c644ca4a2c0d4f6991a490ea36fc48183
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-05 20:36:31 +0200

    ChannelValue.java

--- protocol-java/src/main/java/pl/grzeslowski/jsupla/protocoljava/api/entities/ChannelValue.java
+++ protocol-java/src/main/java/pl/grzeslowski/jsupla/protocoljava/api/entities/ChannelValue.java
@@ -1,5 +1,60 @@
 package pl.grzeslowski.jsupla.protocoljava.api.entities;
 
-// TODO
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
 public class ChannelValue {
+    @NotNull private final pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue value;
+    private final pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue subValue;
+
+    @SuppressWarnings("WeakerAccess")
+    public ChannelValue(final @NotNull pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue value,
+                        final pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue subValue) {
+        this.value = requireNonNull(value);
+        this.subValue = subValue;
+    }
+
+    public ChannelValue(final @NotNull pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue value) {
+        this(value, null);
+    }
+
+    public pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue getValue() {
+        return value;
+    }
+
+    public Optional<pl.grzeslowski.jsupla.protocol.api.channelvalues.ChannelValue> getSubValue() {
+        return Optional.ofNullable(subValue);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ChannelValue)) {
+            return false;
+        }
+
+        final ChannelValue that = (ChannelValue) o;
+
+        if (!value.equals(that.value)) {
+            return false;
+        }
+        return subValue != null ? subValue.equals(that.subValue) : that.subValue == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ChannelValue{" +
+                       "value=" + value +
+                       ", subValue=" + subValue +
+                       '}';
+    }
 }
