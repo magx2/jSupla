c07f41106fd344456f4053271b2d14ad9c65607f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-06 21:43:24 +0200

    DeviceServerEntityParser.java

---
+++ protocol-java/src/main/java/pl/grzeslowski/jsupla/protocoljava/api/parsers/ds/ChannelNewValueResultParser.java
@@ -0,0 +1,8 @@
+package pl.grzeslowski.jsupla.protocoljava.api.parsers.ds;
+
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaChannelNewValueResult;
+import pl.grzeslowski.jsupla.protocoljava.api.entities.ds.ChannelNewValueResult;
+
+public interface ChannelNewValueResultParser
+        extends DeviceServerEntityParser<ChannelNewValueResult, SuplaChannelNewValueResult> {
+}
---
+++ protocol-java/src/main/java/pl/grzeslowski/jsupla/protocoljava/api/parsers/ds/DeviceChannelBParser.java
@@ -0,0 +1,8 @@
+package pl.grzeslowski.jsupla.protocoljava.api.parsers.ds;
+
+import pl.grzeslowski.jsupla.protocol.api.structs.ds.SuplaDeviceChannelB;
+import pl.grzeslowski.jsupla.protocoljava.api.entities.ds.DeviceChannelB;
+import pl.grzeslowski.jsupla.protocoljava.api.parsers.Parser;
+
+public interface DeviceChannelBParser extends Parser<DeviceChannelB, SuplaDeviceChannelB> {
+}
