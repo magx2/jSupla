99e081ead01f16acd7334b3ee7f9eb046577a018
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-09-06 21:55:21 +0200

    ServerDeviceEntityParser.java

---
+++ protocol-java/src/main/java/pl/grzeslowski/jsupla/protocoljava/api/parsers/sd/FirmwareUpdateUrlResultParser.java
@@ -0,0 +1,8 @@
+package pl.grzeslowski.jsupla.protocoljava.api.parsers.sd;
+
+import pl.grzeslowski.jsupla.protocol.api.structs.sd.SuplaFirmwareUpdateUrlResult;
+import pl.grzeslowski.jsupla.protocoljava.api.entities.sd.FirmwareUpdateUrlResult;
+
+public interface FirmwareUpdateUrlResultParser
+        extends ServerDeviceEntityParser<FirmwareUpdateUrlResult, SuplaFirmwareUpdateUrlResult> {
+}
