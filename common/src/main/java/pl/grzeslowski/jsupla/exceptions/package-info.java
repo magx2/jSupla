e33b2228436483bf2ad93eabce99f76d265afd89
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2017-11-15 23:23:59 +0100

    Adding osgi

---
+++ common/src/main/java/pl/grzeslowski/jsupla/exceptions/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package pl.grzeslowski.jsupla.exceptions;
+
+import org.osgi.annotation.versioning.Version;
---
+++ common/src/main/java/pl/grzeslowski/jsupla/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package pl.grzeslowski.jsupla;
+
+import org.osgi.annotation.versioning.Version;
