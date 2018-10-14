ac43c2c2de7780c30d1e574a59876de7aabc7eb3
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-10-14 22:41:34 +0200

    Make SuplaRegisterClientC.java final so equals and hash code would work

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/cs/SuplaRegisterClientC.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/cs/SuplaRegisterClientC.java
@@ -3,12 +3,15 @@ package pl.grzeslowski.jsupla.protocol.api.structs.cs;
 import pl.grzeslowski.jsupla.protocol.api.calltypes.ClientServerCallType;
 
 import static pl.grzeslowski.jsupla.Preconditions.checkArrayLength;
-import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.*;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_CLIENT_NAME_MAXSIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_GUID_SIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_SERVER_NAME_MAXSIZE;
+import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_SOFTVER_MAXSIZE;
 
 /**
  * @since ver. 7
  */
-public class SuplaRegisterClientC implements ClientServer {
+public final class SuplaRegisterClientC implements ClientServer {
     /**
      * UTF-8.
      */
