c3755f0d9b2161a1ebe7da2f9235dc1919780ecc
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-10-18 22:32:40 +0200

    SuplaChannelPackB.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sc/SuplaChannelPackB.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sc/SuplaChannelPackB.java
@@ -1,33 +1,34 @@
 package pl.grzeslowski.jsupla.protocol.api.structs.sc;
 
-import pl.grzeslowski.jsupla.Preconditions;
 import pl.grzeslowski.jsupla.protocol.api.calltypes.ServerClientCallType;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import static pl.grzeslowski.jsupla.Preconditions.checkArrayLength;
-import static pl.grzeslowski.jsupla.protocol.api.calltypes.ServerClientCallType.SUPLA_SC_CALL_CHANNELPACK_UPDATE;
+import static pl.grzeslowski.jsupla.Preconditions.max;
 import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.INT_SIZE;
 import static pl.grzeslowski.jsupla.protocol.api.consts.ProtoConsts.SUPLA_CHANNELPACK_MAXSIZE;
 
-@SuppressWarnings("DeprecatedIsStillUsed")
-@Deprecated
-public final class SuplaChannelPack implements ServerClient {
+/**
+ * @since ver. 8
+ */
+public final class SuplaChannelPackB implements ServerClient {
     public static final int MIN_SIZE = INT_SIZE * 2;
 
     public final int count;
     public final int totalLeft;
-    public final SuplaChannel[] channels;
+    public final SuplaChannelB[] channels;
 
-    public SuplaChannelPack(int count, int totalLeft, SuplaChannel[] channels) {
-        this.count = Preconditions.max(count, SUPLA_CHANNELPACK_MAXSIZE);
+    public SuplaChannelPackB(final int count, final int totalLeft, final SuplaChannelB[] channels) {
+        this.count = max(count, SUPLA_CHANNELPACK_MAXSIZE);
         this.totalLeft = totalLeft;
         this.channels = checkArrayLength(channels, count);
     }
 
     @Override
     public ServerClientCallType callType() {
-        return SUPLA_SC_CALL_CHANNELPACK_UPDATE;
+        return null;
     }
 
     @Override
@@ -37,42 +38,30 @@ public final class SuplaChannelPack implements ServerClient {
 
     private int channelsSize() {
         return Arrays.stream(channels)
-                       .mapToInt(SuplaChannel::size)
+                       .mapToInt(SuplaChannelB::size)
                        .sum();
     }
 
     @Override
     public boolean equals(final Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SuplaChannelPack)) {
-            return false;
-        }
-
-        final SuplaChannelPack that = (SuplaChannelPack) o;
-
-        if (count != that.count) {
-            return false;
-        }
-        if (totalLeft != that.totalLeft) {
-            return false;
-        }
-        // Probably incorrect - comparing Object[] arrays with Arrays.equals
-        return Arrays.equals(channels, that.channels);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final SuplaChannelPackB that = (SuplaChannelPackB) o;
+        return count == that.count &&
+                       totalLeft == that.totalLeft &&
+                       Arrays.equals(channels, that.channels);
     }
 
     @Override
     public int hashCode() {
-        int result = count;
-        result = 31 * result + totalLeft;
+        int result = Objects.hash(count, totalLeft);
         result = 31 * result + Arrays.hashCode(channels);
         return result;
     }
 
     @Override
     public String toString() {
-        return "SuplaChannelPack{" +
+        return "SuplaChannelPackB{" +
                        "count=" + count +
                        ", totalLeft=" + totalLeft +
                        ", channels=" + Arrays.toString(channels) +
