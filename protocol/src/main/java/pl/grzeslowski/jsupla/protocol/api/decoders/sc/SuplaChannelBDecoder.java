84ceb3015b7c6bf288d26061f337044047469bfd
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2018-11-09 01:48:40 +0100

    SuplaChannelBDecoder

---
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/decoders/sc/SuplaChannelBDecoder.java
@@ -0,0 +1,6 @@
+package pl.grzeslowski.jsupla.protocol.api.decoders.sc;
+
+import pl.grzeslowski.jsupla.protocol.api.structs.sc.SuplaChannelB;
+
+public interface SuplaChannelBDecoder extends ServerClientDecoder<SuplaChannelB> {
+}
