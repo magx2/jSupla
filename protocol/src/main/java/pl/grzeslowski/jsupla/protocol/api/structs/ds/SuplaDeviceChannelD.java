f41cbadf9f8ec70f77b145a3862adfa72477970d
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-04-29 18:19:25 +0200

    SuplaDeviceChannelD

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/ds/SuplaDeviceChannelD.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/ds/SuplaDeviceChannelD.java
@@ -1,12 +1,105 @@
 package pl.grzeslowski.jsupla.protocol.api.structs.ds;
 
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts;
+import pl.grzeslowski.jsupla.protocol.api.structs.ActionTriggerProperties;
+import pl.grzeslowski.jsupla.protocol.api.structs.HVACValue;
+import pl.grzeslowski.jsupla.protocol.api.structs.Union;
 import pl.grzeslowski.jsupla.protocol.api.types.ProtoWithSize;
 
+import static pl.grzeslowski.jsupla.Preconditions.union;
+import static pl.grzeslowski.jsupla.Preconditions.*;
+import static pl.grzeslowski.jsupla.protocol.api.consts.JavaConsts.*;
+
+@EqualsAndHashCode
+@ToString
 public final class SuplaDeviceChannelD implements ProtoWithSize {
-    public static final int SIZE = 1;
+    public static final int SIZE =
+        BYTE_SIZE + //number
+            INT_SIZE + //type
+            INT_SIZE + // union 1 / funcList | actionTriggerCaps
+            INT_SIZE + // defaultValue 
+            LONG_SIZE + // flags 
+            BYTE_SIZE + //offline
+            INT_SIZE + // valueValidityTimeSec
+            JavaConsts.union(BYTE_SIZE, ActionTriggerProperties.SIZE, HVACValue.SIZE) + // union 2 / value | actionTriggerProperties | hvacValue
+            BYTE_SIZE // defaultIcon
+        ;
+
+    /**
+     * unsigned char
+     */
+    public final short number;
+    public final int type;
+    @Union(groupNumber = 1)
+    public final Integer funcList;
+    /**
+     * unsigned
+     */
+    @Union(groupNumber = 1)
+    public final Long actionTriggerCaps;
+    /**
+     * This field name should be default, but this is keyword in Java.
+     */
+    public final int defaultValue;
+    public final long flags;
+
+    /**
+     * unsigned
+     */
+    public final short offline;
+    /**
+     * unsigned
+     */
+    public final long valueValidityTimeSec;
+
+    @Union(groupNumber = 2)
+    public final byte[] value;
+    @Union(groupNumber = 2)
+    public final ActionTriggerProperties actionTriggerProperties;
+    @Union(groupNumber = 2)
+    public final HVACValue hvacValue;
+
+    /**
+     * unsigned
+     */
+    public final short defaultIcon;
+
+    public SuplaDeviceChannelD(short number,
+                               int type,
+                               Integer funcList,
+                               Long actionTriggerCaps,
+                               int defaultValue,
+                               long flags,
+                               short offline,
+                               long valueValidityTimeSec,
+                               byte[] value,
+                               ActionTriggerProperties actionTriggerProperties,
+                               HVACValue hvacValue,
+                               short defaultIcon) {
+        this.number = unsignedByteSize(number);
+        this.type = type;
+
+        this.funcList = funcList;
+        this.actionTriggerCaps = unsignedIntSize(actionTriggerCaps);
+        union(funcList, actionTriggerCaps);
+
+        this.defaultValue = defaultValue;
+        this.flags = flags;
+        this.offline = unsigned(offline);
+        this.valueValidityTimeSec = unsigned(valueValidityTimeSec);
+
+        this.value = value;
+        this.actionTriggerProperties = actionTriggerProperties;
+        this.hvacValue = hvacValue;
+        union(value, actionTriggerProperties, hvacValue);
+
+        this.defaultIcon = unsignedByteSize(defaultIcon);
+    }
 
     @Override
     public int size() {
-        return 0;
+        return SIZE;
     }
 }
