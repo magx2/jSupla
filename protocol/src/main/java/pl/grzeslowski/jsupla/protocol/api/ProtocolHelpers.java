3f9663f5d6398ad2f0e5ca89fb560977c20dd18b
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-04-30 02:02:06 +0200

    Add more methods to ProtocolHelpers.java

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/ProtocolHelpers.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/ProtocolHelpers.java
@@ -1,6 +1,12 @@
 package pl.grzeslowski.jsupla.protocol.api;
 
+import lombok.val;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static pl.grzeslowski.jsupla.Preconditions.checkArrayLength;
 
 public interface ProtocolHelpers {
     static String parseString(byte[] bytes) {
@@ -12,5 +18,38 @@ public interface ProtocolHelpers {
             }
         }
         return new String(bytes, 0, length, UTF_8);
+    }
+
+    /**
+     * TODO can be optimized!!!.
+     *
+     * @see <a href="https://stackoverflow.com/a/4932112/1819402">https://stackoverflow.com/a/4932112/1819402</a>
+     */
+    static char[] parsePassword(final byte[] utfBytes) {
+        final CharBuffer charBuffer = UTF_8.decode(ByteBuffer.wrap(utfBytes)); // also decode to String
+        return charBuffer.array();
+    }
+
+    static String parseHexString(byte[] bytes) {
+        val HEX_LENGTH = 16;
+        checkArrayLength(bytes, HEX_LENGTH);
+        return String.format("%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
+            bytes[0],
+            bytes[1],
+            bytes[2],
+            bytes[3],
+            bytes[4],
+            bytes[5],
+            bytes[6],
+            bytes[7],
+            bytes[8],
+            bytes[9],
+            bytes[10],
+            bytes[11],
+            bytes[12],
+            bytes[13],
+            bytes[14],
+            bytes[15]
+        );
     }
 }
