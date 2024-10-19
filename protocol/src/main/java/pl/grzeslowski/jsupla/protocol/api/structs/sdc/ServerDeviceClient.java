8dcfc3547639ac64e6f4ca9d4cc8efe41d52ad5f
Martin Grześlowski <martin.grzeslowski@gmail.com>
2024-10-19 11:26:13 +0200

    Fixes int from/to proto

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sdc/ServerDeviceClient.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/structs/sdc/ServerDeviceClient.java
@@ -1,14 +1,13 @@
 package pl.grzeslowski.jsupla.protocol.api.structs.sdc;
 
 import pl.grzeslowski.jsupla.protocol.api.calltypes.ServerDeviceClientCallType;
-import pl.grzeslowski.jsupla.protocol.api.types.FromDeviceProto;
 import pl.grzeslowski.jsupla.protocol.api.types.FromServerProto;
+import pl.grzeslowski.jsupla.protocol.api.types.ToClientProto;
 import pl.grzeslowski.jsupla.protocol.api.types.ToDeviceProto;
-import pl.grzeslowski.jsupla.protocol.api.types.ToServerProto;
 
 /**
  * Structs send from server to device/client.
  */
-public interface ServerDeviceClient extends FromServerProto, ToDeviceProto, FromDeviceProto, ToServerProto {
+public interface ServerDeviceClient extends FromServerProto, ToDeviceProto, ToClientProto {
     ServerDeviceClientCallType callType();
 }
