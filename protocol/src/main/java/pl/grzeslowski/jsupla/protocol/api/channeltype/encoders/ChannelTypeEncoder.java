d23a52808215de1f590d77488d6dd96c90f8b718
Martin Grzeslowski <martin@grzeslowski.pl>
2026-07-31 11:33:33 +0200

    Use semantic relay encoder for gate values

--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channeltype/encoders/ChannelTypeEncoder.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channeltype/encoders/ChannelTypeEncoder.java
@@ -13,7 +13,6 @@ public class ChannelTypeEncoder {
     private final HumidityTypeEncoder humidityTypeEncoder;
     private final HvacTypeEncoder hvacTypeEncoder;
     private final OnOffTypeEncoder onOffTypeEncoder;
-    private final GateTypeEncoder gateTypeEncoder;
     private final SemanticRelayValueTypeEncoder semanticRelayValueTypeEncoder;
     private final PercentTypeEncoder percentTypeEncoder;
     private final ThermometerDoubleTypeEncoder thermometerDoubleTypeEncoder;
@@ -30,7 +29,6 @@ public class ChannelTypeEncoder {
                 new HumidityTypeEncoder(),
                 new HvacTypeEncoder(),
                 new OnOffTypeEncoder(),
-                new GateTypeEncoder(),
                 new SemanticRelayValueTypeEncoder(),
                 new PercentTypeEncoder(),
                 new ThermometerDoubleTypeEncoder(),
@@ -47,37 +45,8 @@ public class ChannelTypeEncoder {
             case HumidityValue humidityValue -> humidityTypeEncoder.encode(humidityValue);
             case HvacValue hvacValue -> hvacTypeEncoder.encode(hvacValue);
             case OnOffValue onOffValue -> onOffTypeEncoder.encode(onOffValue);
-            case GateValue gateValue -> gateTypeEncoder.encode(gateValue);
-            case CurtainValue curtainValue -> semanticRelayValueTypeEncoder.encode(curtainValue);
-            case DoorLockValue doorLockValue -> semanticRelayValueTypeEncoder.encode(doorLockValue);
-            case FacadeBlindValue facadeBlindValue ->
-                    semanticRelayValueTypeEncoder.encode(facadeBlindValue);
-            case GarageDoorValue garageDoorValue ->
-                    semanticRelayValueTypeEncoder.encode(garageDoorValue);
-            case GatewayLockValue gatewayLockValue ->
-                    semanticRelayValueTypeEncoder.encode(gatewayLockValue);
-            case HeatOrColdSourceSwitchValue heatOrColdSourceSwitchValue ->
-                    semanticRelayValueTypeEncoder.encode(heatOrColdSourceSwitchValue);
-            case LightSwitchValue lightSwitchValue ->
-                    semanticRelayValueTypeEncoder.encode(lightSwitchValue);
-            case PowerSwitchValue powerSwitchValue ->
-                    semanticRelayValueTypeEncoder.encode(powerSwitchValue);
-            case ProjectorScreenValue projectorScreenValue ->
-                    semanticRelayValueTypeEncoder.encode(projectorScreenValue);
-            case PumpSwitchValue pumpSwitchValue ->
-                    semanticRelayValueTypeEncoder.encode(pumpSwitchValue);
-            case RollerGarageDoorValue rollerGarageDoorValue ->
-                    semanticRelayValueTypeEncoder.encode(rollerGarageDoorValue);
-            case RollerShutterValue rollerShutterValue ->
-                    semanticRelayValueTypeEncoder.encode(rollerShutterValue);
-            case RoofWindowValue roofWindowValue ->
-                    semanticRelayValueTypeEncoder.encode(roofWindowValue);
-            case StaircaseTimerValue staircaseTimerValue ->
-                    semanticRelayValueTypeEncoder.encode(staircaseTimerValue);
-            case TerraceAwningValue terraceAwningValue ->
-                    semanticRelayValueTypeEncoder.encode(terraceAwningValue);
-            case VerticalBlindValue verticalBlindValue ->
-                    semanticRelayValueTypeEncoder.encode(verticalBlindValue);
+            case AbstractOnOffValue abstractOnOffValue ->
+                    semanticRelayValueTypeEncoder.encode(abstractOnOffValue);
             case PercentValue percentValue -> percentTypeEncoder.encode(percentValue);
             case RgbValue rgbValue -> rgbTypeEncoder.encode(rgbValue);
             case TemperatureAndHumidityValue temperatureAndHumidityValue ->
--- protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channeltype/encoders/SemanticRelayValueTypeEncoder.java
+++ protocol/src/main/java/pl/grzeslowski/jsupla/protocol/api/channeltype/encoders/SemanticRelayValueTypeEncoder.java
@@ -1,44 +1,11 @@
 package pl.grzeslowski.jsupla.protocol.api.channeltype.encoders;
 
-import pl.grzeslowski.jsupla.protocol.api.channeltype.value.*;
+import pl.grzeslowski.jsupla.protocol.api.channeltype.value.AbstractOnOffValue;
+import pl.grzeslowski.jsupla.protocol.api.channeltype.value.OnOffValue;
 
-class SemanticRelayValueTypeEncoder implements ChannelValueEncoder<ChannelValue> {
+class SemanticRelayValueTypeEncoder implements ChannelValueEncoder<AbstractOnOffValue> {
     @Override
-    public void encode(final ChannelValue value, final byte[] bytes) {
-        bytes[0] = isActive(value) ? (byte) 1 : (byte) 0;
-    }
-
-    private boolean isActive(final ChannelValue value) {
-        return switch (value) {
-            case CurtainValue curtainValue -> curtainValue == CurtainValue.OPEN;
-            case DoorLockValue doorLockValue -> doorLockValue == DoorLockValue.UNLOCKED;
-            case FacadeBlindValue facadeBlindValue -> facadeBlindValue == FacadeBlindValue.OPEN;
-            case GarageDoorValue garageDoorValue -> garageDoorValue == GarageDoorValue.OPEN;
-            case GateValue gateValue -> gateValue == GateValue.OPEN;
-            case GatewayLockValue gatewayLockValue -> gatewayLockValue == GatewayLockValue.UNLOCKED;
-            case HeatOrColdSourceSwitchValue heatOrColdSourceSwitchValue ->
-                    heatOrColdSourceSwitchValue == HeatOrColdSourceSwitchValue.ON;
-            case LightSwitchValue lightSwitchValue -> lightSwitchValue == LightSwitchValue.ON;
-            case PowerSwitchValue powerSwitchValue -> powerSwitchValue == PowerSwitchValue.ON;
-            case ProjectorScreenValue projectorScreenValue ->
-                    projectorScreenValue == ProjectorScreenValue.OPEN;
-            case PumpSwitchValue pumpSwitchValue -> pumpSwitchValue == PumpSwitchValue.ON;
-            case RollerGarageDoorValue rollerGarageDoorValue ->
-                    rollerGarageDoorValue == RollerGarageDoorValue.OPEN;
-            case RollerShutterValue rollerShutterValue ->
-                    rollerShutterValue == RollerShutterValue.OPEN;
-            case RoofWindowValue roofWindowValue -> roofWindowValue == RoofWindowValue.OPEN;
-            case StaircaseTimerValue staircaseTimerValue ->
-                    staircaseTimerValue == StaircaseTimerValue.ON;
-            case TerraceAwningValue terraceAwningValue ->
-                    terraceAwningValue == TerraceAwningValue.OPEN;
-            case VerticalBlindValue verticalBlindValue ->
-                    verticalBlindValue == VerticalBlindValue.OPEN;
-            default ->
-                    throw new UnsupportedOperationException(
-                            "SemanticRelayValueTypeEncoder.encode("
-                                    + value.getClass().getSimpleName()
-                                    + ")");
-        };
+    public void encode(final AbstractOnOffValue value, final byte[] bytes) {
+        bytes[0] = value.toCommonBase() == OnOffValue.ON ? (byte) 1 : (byte) 0;
     }
 }
